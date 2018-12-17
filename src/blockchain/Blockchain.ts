42ac49ca59e7f36d57ce8f86f622aa11360210e4
Mario Neises <git@mnio.net>
2018-12-17 11:01:54 +0100

    feat(Blockchain): migrate code from client to SDK

--- src/blockchain/Blockchain.ts
+++ src/blockchain/Blockchain.ts
@@ -5,9 +5,10 @@ import { ApiPromise } from '@polkadot/api'
 import { WsProvider } from '@polkadot/rpc-provider'
 
 export default class Blockchain {
-  public static async connect (
-    host: string = 'ws://127.0.0.1:9944'
-  ): Promise<ApiPromise> {
+
+  public static DEFAULT_WS_ADDRESS = 'ws://127.0.0.1:9944'
+
+  public static async connect (host: string = Blockchain.DEFAULT_WS_ADDRESS): Promise<ApiPromise> {
     const provider = new WsProvider(host)
     const api = await ApiPromise.create(provider)
     return api
