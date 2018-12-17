49245640cd72235a12cd4f7549542dff52b64d03
Timo Welde <tjwelde@gmail.com>
2018-12-17 12:13:01 +0100

    refactor: refactored crypto to use named exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as Crypto from './crypto/Crypto'
+
 export { default as Demo } from './demo/Demo'
-export { default as Crypto } from './crypto/Crypto'
 export { default as Identity } from './identity/Identity'
+export { Crypto }
