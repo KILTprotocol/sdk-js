8ab5a068009ce5b8ea843861ea77b0fec174ed35
Mario Neises <git@mnio.net>
2018-12-18 10:37:08 +0100

    refactor: apply prettier style, use fixed dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { default as Demo } from './demo/Demo'
 export { default as Identity } from './identity/Identity'
 export { default as Crypto } from './crypto'
-export { CTypeModel, CTypeInputModel, CTypeWrapperModel } from './ctype/CTypeSchema'
-
+export {
+  CTypeModel,
+  CTypeInputModel,
+  CTypeWrapperModel,
+} from './ctype/CTypeSchema'
