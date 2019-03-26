575f6d1545b1f53213b7517fa91a93d33433a4a2
Benjamin Kolb <benjamin.kolb@sprylab.com>
2019-03-26 10:39:17 +0100

    feat: did

---
+++ src/did/Did.ts
@@ -0,0 +1,131 @@
+import Identity from '../identity/Identity'
+import Blockchain, { QueryResult } from '../blockchain/Blockchain'
+import { IPublicIdentity } from '../identity/PublicIdentity'
+import { factory } from '../config/ConfigLog'
+import { TxStatus } from '../blockchain/TxStatus'
+import SubmittableExtrinsic from '@polkadot/api/SubmittableExtrinsic'
+import { CodecResult } from '@polkadot/api/promise/types'
+
+const log = factory.getLogger('DID')
+
+export const DID_IDENTIFIER_PREFIX = 'did:kilt:'
+
+export interface IDid {
+  identifier: string
+  publicBoxKey: string
+  publicSigningKey: string
+  documentStore?: string
+}
+
+export default class Did implements IDid {
+  public readonly identifier: string
+  public readonly publicBoxKey: string
+  public readonly publicSigningKey: string
+  public readonly documentStore?: string
+
+  private constructor(
+    identifier: string,
+    publicBoxKey: string,
+    publicSigningKey: string,
+    documentStore?: string
+  ) {
+    this.identifier = identifier
+    this.publicBoxKey = publicBoxKey
+    this.publicSigningKey = publicSigningKey
+    this.documentStore = documentStore
+  }
+
+  public async store(
+    blockchain: Blockchain,
+    identity: Identity
+  ): Promise<TxStatus> {
+    log.debug(() => `Create tx for 'did.add'`)
+    const tx: SubmittableExtrinsic<
+      CodecResult,
+      any
+    > = await blockchain.api.tx.did.add(
+      this.publicBoxKey,
+      this.publicSigningKey,
+      this.documentStore
+    )
+    return blockchain.submitTx(identity, tx)
+  }
+
+  public async remove(
+    blockchain: Blockchain,
+    identity: Identity
+  ): Promise<TxStatus> {
+    log.debug(() => `Create tx for 'did.remove'`)
+    const tx: SubmittableExtrinsic<
+      CodecResult,
+      any
+    > = await blockchain.api.tx.did.remove()
+    return blockchain.submitTx(identity, tx)
+  }
+
+  public static decodeDid(
+    identifier: string,
+    encoded: QueryResult
+  ): IDid | undefined {
+    const json = encoded && encoded.encodedLength ? encoded.toJSON() : null
+    let result: IDid | undefined
+    if (json instanceof Array) {
+        result = Object.assign(Object.create(Did.prototype), {
+        identifier: identifier,
+        publicBoxKey: json[0],
+        publicSigningKey: json[1],
+        documentStore: json[2],
+      } as IDid)
+    }
+    return result
+  }
+
+  public static async queryByIdentifier(
+    blockchain: Blockchain,
+    identifier: IDid['identifier']
+  ): Promise<IDid | undefined> {
+    const address = Did.getAddressFromIdentifier(identifier)
+    const decoded: IDid | undefined = Did.decodeDid(
+      identifier,
+      await blockchain.api.query.dids.dIDs(address)
+    )
+    return decoded
+  }
+
+  public static async queryByAddress(
+    blockchain: Blockchain,
+    address: IPublicIdentity['address']
+  ): Promise<IDid | undefined> {
+    const identifier = Did.getIdentifierFromAddress(address)
+    const decoded: IDid | undefined = Did.decodeDid(
+      identifier,
+      await blockchain.api.query.dids.dIDs(address)
+    )
+    return decoded
+  }
+
+  public static fromIdentity(identity: Identity, documentStore?: string): Did {
+    const identifier = Did.getIdentifierFromAddress(identity.address)
+    return new Did(
+      identifier,
+      identity.boxPublicKeyAsHex,
+      identity.signPublicKeyAsHex,
+      documentStore
+    )
+  }
+
+  public static getIdentifierFromAddress(
+    address: IPublicIdentity['address']
+  ): IDid['identifier'] {
+    return DID_IDENTIFIER_PREFIX + address
+  }
+
+  public static getAddressFromIdentifier(
+    identifier: IDid['identifier']
+  ): IPublicIdentity['address'] {
+    if (!identifier.startsWith(DID_IDENTIFIER_PREFIX)) {
+      throw new Error('Not a KILT did: ' + identifier)
+    }
+    return identifier.substr(DID_IDENTIFIER_PREFIX.length)
+  }
+}
