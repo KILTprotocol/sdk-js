105d81e865e53ef0e0757318245e62783aae7595
Timo Welde <tjwelde@gmail.com>
2019-05-03 18:17:50 +0200

    refactor: no unnecessary classes

--- src/util/UUID.ts
+++ src/util/UUID.ts
@@ -1,11 +1,13 @@
 import { v4 as uuid } from 'uuid'
 import { Hash } from '@polkadot/types'
 
-export class UUID {
-  /**
-   * Generates a H256 compliant UUID.
-   */
-  public static generate(): string {
-    return new Hash(uuid()).toString()
-  }
+/**
+ * Generates a H256 compliant UUID.
+ */
+export function generate(): string {
+  return new Hash(uuid()).toString()
+}
+
+export default {
+  generate,
 }
