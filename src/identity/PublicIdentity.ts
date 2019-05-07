f0baff501bff6b6add40c32603118a7b88d2540c
Benjamin Kolb <benjamin.kolb@sprylab.com>
2019-05-07 16:12:16 +0200

    fix: url resolver returns promise

--- src/identity/PublicIdentity.ts
+++ src/identity/PublicIdentity.ts
@@ -5,7 +5,7 @@ import Blockchain from '../blockchain/Blockchain'
 import IPublicIdentity from '../types/PublicIdentity'
 
 export interface IURLResolver {
-  resolve(url: string): object
+  resolve(url: string): Promise<object | undefined>
 }
 
 /**
@@ -51,10 +51,11 @@ export default class PublicIdentity implements IPublicIdentity {
         identifier
       )
       if (did !== undefined) {
-        if (did.documentStore) {
-          return this.fromDidDocument(
-            await urlResolver.resolve(did.documentStore)
-          )
+        const didDocument: object | undefined = did.documentStore
+          ? await urlResolver.resolve(did.documentStore)
+          : undefined
+        if (didDocument) {
+          return this.fromDidDocument(didDocument)
         } else {
           return new PublicIdentity(
             Did.getAddressFromIdentifier(did.identifier),
@@ -68,7 +69,7 @@ export default class PublicIdentity implements IPublicIdentity {
           encodeURIComponent(identifier)
       )
       /* tslint:disable:no-string-literal */
-      if (didResult['didDocument']) {
+      if (didResult && didResult['didDocument']) {
         return this.fromDidDocument(didResult['didDocument'])
       }
       /* tslint:enable:no-string-literal */
