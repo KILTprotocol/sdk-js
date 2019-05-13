752b1b7fe42a5ad2d47613c14830220ea3542a8c
Andreas Pasch <foby@gmx.net>
2019-05-13 16:48:29 +0200

    feat: document Balance.chain.ts

--- src/balance/Balance.chain.ts
+++ src/balance/Balance.chain.ts
@@ -1,13 +1,37 @@
 import BN from 'bn.js'
-
+import { TxStatus } from '../blockchain/TxStatus'
 import { getCached } from '../blockchainApiConnection'
 import Identity from '../identity/Identity'
 import IPublicIdentity from '../types/PublicIdentity'
-import { TxStatus } from '../blockchain/TxStatus'
 
+/**
+ * @description Attaches the given [listener] for balance changes on the account with [accountAddress].
+ * <B>Note that balance amount is in µ-Kilt and must be translated to Kilt-Coin</B>
+ *
+ * @param accountAddress address of the account on which to listen for balance changes.
+ * @param listener listener to receive balance change updates
+ * @returns a promise containing the current balance of the account
+ *
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import * as sdk from '@kiltprotocol/prototype-sdk';
+ *
+ * const address = ...
+ * sdk.Balance.listenToBalanceChanges(address,
+ *   (account: IPublicIdentity['address'], balance: BN, change: BN) => {
+ *     console.log(`Balance has changed by ${change.toNumber()} to ${balance.toNumber()}`)
+ *   });
+ * ```
+ */
 export async function listenToBalanceChanges(
-  accountAddress: string,
-  listener?: (account: string, balance: BN, change: BN) => void
+  accountAddress: IPublicIdentity['address'],
+  listener?: (
+    account: IPublicIdentity['address'],
+    balance: BN,
+    change: BN
+  ) => void
 ): Promise<BN> {
   const blockchain = await getCached()
   // @ts-ignore
@@ -25,20 +49,62 @@ export async function listenToBalanceChanges(
   return previous
 }
 
+/**
+ * @description Fetches the current balance of the account with [accountAddress].
+ * <B>Note that balance amount is in µ-Kilt and must be translated to Kilt-Coin</B>
+ *
+ * @param accountAddress address of the account for which to get the balance.
+ * @returns a promise containing the current balance of the account
+ *
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import * as sdk from '@kiltprotocol/prototype-sdk';
+ *
+ * const address = ...
+ * sdk.Balance.getBalance(address)
+ *   .then((balance: BN) => {
+ *     console.log(`balance is ${balance.toNumber()}`)
+ *   })
+ * ```
+ */
 export async function getBalance(
   accountAddress: IPublicIdentity['address']
 ): Promise<BN> {
-  const blockchain = await getCached()
-  // @ts-ignore
-  const balance: BN = await blockchain.api.query.balances.freeBalance(
-    accountAddress
-  )
-  return balance
+  return listenToBalanceChanges(accountAddress)
 }
 
+/**
+ * @description Transfer Kilt [amount] tokens to [toAccountAddress] using the given [[Identity]].
+ * <B>Note that balance amount is in µ-Kilt and must be translated to Kilt-Coin</B>
+ *
+ * @param identity identity to use for token transfer
+ * @param accountAddressTo address of the receiver account
+ * @param amount amount of µ-Kilt to transfer
+ * @returns promise containing the transaction status
+ *
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import * as sdk from '@kiltprotocol/prototype-sdk';
+ *
+ * const identity = ...
+ * const address = ...
+ * const amount: BN = new BN(42)
+ * sdk.Balance.makeTransfer(identity, address, amount)
+ *   .then((status: TxStatus) => {
+ *     console.log('Successfully transferred ${amount.toNumber()} tokens')
+ *   })
+ *   .catch(err => {
+ *     console.log('Transfer failed')
+ *   })
+ * ```
+ */
 export async function makeTransfer(
   identity: Identity,
-  accountAddressTo: string,
+  accountAddressTo: IPublicIdentity['address'],
   amount: BN
 ): Promise<TxStatus> {
   const blockchain = await getCached()
