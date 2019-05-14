655cfc0fd8302b6a2ccf4118b0f9b337ada4204f
Andreas Pasch <foby@gmx.net>
2019-05-14 15:59:13 +0200

    feat: document DelegationNode.ts

--- src/delegation/DelegationNode.ts
+++ src/delegation/DelegationNode.ts
@@ -16,6 +16,12 @@ const log = factory.getLogger('DelegationNode')
 
 export default class DelegationNode extends DelegationBaseNode
   implements IDelegationNode {
+  /**
+   * @description Queries the delegation node with [delegationId].
+   *
+   * @param delegationId the unique identifier of the desired delegation
+   * @returns promise containing the [[DelegationNode]] or [undefined]
+   */
   public static async query(delegationId: string) {
     log.info(`:: query('${delegationId}')`)
     const result = await query(delegationId)
@@ -27,6 +33,15 @@ export default class DelegationNode extends DelegationBaseNode
   public parentId?: IDelegationNode['parentId']
   public permissions: IDelegationNode['permissions']
 
+  /**
+   * @description Creates a new [DelegationNode].
+   *
+   * @param id a unique identifier
+   * @param rootId identifier of the root delegation node that is already stored on-chain
+   * @param account address of the account that will be the owner of the delegation
+   * @param permissions list of [[Permission]]s
+   * @param parentId identifier of the parent delegation node already stored on-chain. Not required when the parent is the root node.
+   */
   constructor(
     id: IDelegationNode['id'],
     rootId: IDelegationNode['rootId'],
@@ -40,6 +55,25 @@ export default class DelegationNode extends DelegationBaseNode
     this.parentId = parentId
   }
 
+  /**
+   * @description Generates the delegation hash from the delegations' property values.
+   *
+   * <BR>
+   * This hash is signed by the delegate and later stored along with the delegation to
+   * make sure delegation data (such as permissions) is not tampered.
+   *
+   * @example
+   * <BR>
+   * ```
+   * const delegate: Identity = ...
+   * const signature:string = delegate.signStr(newDelegationNode.generateHash())
+   *
+   * const myIdentity: Identity = ...
+   * newDelegationNode.store(myIdentity, signature)
+   * ```
+   *
+   * @returns the hash representation of this delegation as a hex string
+   */
   public generateHash(): string {
     const propsToHash: Array<Uint8Array | string> = [this.id, this.rootId]
     if (this.parentId && this.parentId !== this.rootId) {
@@ -56,6 +90,10 @@ export default class DelegationNode extends DelegationBaseNode
     return generated
   }
 
+  /**
+   * @description Fetches the root of this delegation node.
+   * @returns promise containing the [[DelegationRootNode]] of this delegation node
+   */
   public async getRoot(): Promise<DelegationRootNode> {
     const rootNode = await queryRoot(this.rootId)
     if (!rootNode) {
@@ -63,6 +101,11 @@ export default class DelegationNode extends DelegationBaseNode
     }
     return rootNode
   }
+
+  /**
+   * @description Fetches the parent node of this delegation node.
+   * @returns promise containing the parent as [[DelegationBaseNode]] or [undefined]
+   */
 
   public async getParent(): Promise<DelegationBaseNode | undefined> {
     if (!this.parentId) {
@@ -72,23 +115,35 @@ export default class DelegationNode extends DelegationBaseNode
     return await query(this.parentId)
   }
 
+  /**
+   * @description Stores the delegation node on chain.
+   *
+   * @param identity account used to store the delegation node
+   * @param signature signature of the delegate to ensure it's done under his permission
+   * @returns promise containing the [[TxStatus]]
+   */
   public async store(identity: Identity, signature: string): Promise<TxStatus> {
     log.info(`:: store(${this.id})`)
     return store(this, identity, signature)
   }
 
+  /**
+   * @description Verifies the delegation node by querying it from chain and checking its revoke status.
+   * @returns promise containing a boolean flag
+   */
   public async verify(): Promise<boolean> {
     const node: DelegationNode | undefined = await query(this.id)
     return node !== undefined && !node.revoked
   }
 
+  /**
+   * @description Revokes the delegation node on chain.
+   * @param identity the identity used to revoke the delegation
+   * @returns promise containing the [[TxStatus]]
+   */
   public async revoke(identity: Identity): Promise<TxStatus> {
     log.debug(`:: revoke(${this.id})`)
     return revoke(this.id, identity)
-  }
-
-  public permissionsAsBitset() {
-    return permissionsAsBitset(this)
   }
 
   protected decodeChildNode(
