06d85c67689083d1f147777da646b7ad434414d0
Andreas Pasch <foby@gmx.net>
2019-05-14 17:26:33 +0200

    feat: document DelegationRootNode.ts, Did.ts

--- src/delegation/DelegationRootNode.ts
+++ src/delegation/DelegationRootNode.ts
@@ -1,17 +1,23 @@
-import { QueryResult } from '../blockchain/Blockchain'
 import { TxStatus } from '../blockchain/TxStatus'
 import { factory } from '../config/ConfigLog'
 import Identity from '../identity/Identity'
-import DelegationBaseNode from './Delegation'
-import { decodeDelegationNode } from './DelegationDecoder'
 import { IDelegationRootNode } from '../types/Delegation'
+import DelegationBaseNode from './Delegation'
+import { query, revoke, store } from './DelegationRootNode.chain'
 import DelegationNode from './DelegationNode'
-import { store, query, revoke } from './DelegationRootNode.chain'
+import { QueryResult } from '../blockchain/Blockchain'
+import { decodeDelegationNode } from './DelegationDecoder'
 
 const log = factory.getLogger('DelegationRootNode')
 
 export default class DelegationRootNode extends DelegationBaseNode
   implements IDelegationRootNode {
+  /**
+   * @description Queries the delegation root with [delegationId].
+   *
+   * @param delegationId unique identifier of the delegation root
+   * @returns promise containing [[DelegationRootNode]] or [undefined]
+   */
   public static async query(delegationId: string) {
     log.info(`:: query('${delegationId}')`)
     const result = await query(delegationId)
@@ -38,21 +44,34 @@ export default class DelegationRootNode extends DelegationBaseNode
   public getRoot(): Promise<DelegationRootNode> {
     return Promise.resolve(this)
   }
+
   // tslint:disable-next-line:prefer-function-over-method
   public getParent(): Promise<DelegationBaseNode | undefined> {
     return Promise.resolve(undefined)
   }
 
+  /**
+   * @description Stores the delegation root node on chain.
+   *
+   * @param identity the account used to store the delegation root node
+   * @returns promise containing the [[TxStatus]]
+   */
   public async store(identity: Identity): Promise<TxStatus> {
     log.debug(`:: store(${this.id})`)
     return store(this, identity)
   }
 
+  /**
+   * @see [[DelegationBaseNode#verify]]
+   */
   public async verify(): Promise<boolean> {
     const node: IDelegationRootNode | undefined = await query(this.id)
     return node !== undefined && !node.revoked
   }
 
+  /**
+   * @see [[DelegationBaseNode#revoke]]
+   */
   public async revoke(identity: Identity): Promise<TxStatus> {
     log.debug(`:: revoke(${this.id})`)
     return revoke(this, identity)
