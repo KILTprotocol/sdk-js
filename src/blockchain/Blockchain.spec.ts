085d152d857495bbc339ca50d0fe16699c47ac31
Timo Welde <tjwelde@gmail.com>
2019-06-07 16:09:43 +0200

    test: fix method call in blockchain test

--- src/blockchain/Blockchain.spec.ts
+++ src/blockchain/Blockchain.spec.ts
@@ -1,11 +1,14 @@
 import { Header } from '@polkadot/types'
 import { IBlockchainApi } from './Blockchain'
+import { getCached } from '../blockchainApiConnection'
 // NB: see jest.config.js - include this dir to be tested for test coverage again
 // to acquire a connection as singleton, async and without jest complaining about
 // 'Jest: Coverage data for ./src/blockchain/ was not found.' I use this construct for now
 let blockchainSingleton: IBlockchainApi
 
 describe('Blockchain', async () => {
+  blockchainSingleton = await getCached()
+
   xit('should get stats', async () => {
     const stats = await blockchainSingleton.getStats()
     expect(stats).toEqual({
