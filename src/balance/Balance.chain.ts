3eb10d93d5b044380d8f9400f6239042ea3a960a
dudleyneedham <dudleyneedham1993@gmail.com>
2019-08-27 11:17:49 +0200

    docs(api): balances adding overview and usage

--- src/balance/Balance.chain.ts
+++ src/balance/Balance.chain.ts
@@ -1,5 +1,15 @@
 /**
  * @module Balance
+ *
+ *  --- Overview ---
+ *
+ *  The balance module provides the accounts and balance of the KILT protocol.
+ *
+ *  --- Usage ---
+ *
+ *  Checking Balances between accounts
+ *  Transfer of assets between accounts
+ *
  */
 
 import BN from 'bn.js'
