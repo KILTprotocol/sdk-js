91e5dbd0010c195481dca58eddd49c05aad6ff08
maudnals <maud.nalpas@gmail.com>
2019-09-02 16:58:05 +0200

    docs(): refined

--- src/claim/Claim.ts
+++ src/claim/Claim.ts
@@ -1,9 +1,7 @@
 /**
- * Claims are a core building block of the KILT SDK. A claim represents **something an entity claims about itself**.
- * <br>
- * Once created, a claim can be used to create a [[RequestForAttestation]].
- * ---
- * A claim object has: 
+ * Claims are a core building block of the KILT SDK. A claim represents **something an entity claims about itself**. Once created, a claim can be used to create a [[RequestForAttestation]].
+ * ***
+ * A claim object has:
  * * contents - among others, the pure content of a claim, for example `"isOver18": yes`;
  * * a [[CType]] that represents its data structure.
  * <br><br>
