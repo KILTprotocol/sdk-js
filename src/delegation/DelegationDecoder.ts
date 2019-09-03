fe473a3d5c60643a3f3c82caea75d11f44c1e499
maudnals <maud.nalpas@gmail.com>
2019-09-03 14:44:49 +0200

    docs(): refined

--- src/delegation/DelegationDecoder.ts
+++ src/delegation/DelegationDecoder.ts
@@ -1,11 +1,8 @@
 /**
- *
- * Delegation Decoder functions to decode types queried from the chain.
- *
- * ---
- *
- * When a type is queried from the chain using the `api.query...` functions, a result of type `Codec` is returned
- * by the polkadot-js api. We need to decode the encoded data to build the Kilt types from it.
+ * When [[DelegationNode]]s or [[DelegationRootNode]]s are written on the blockchain, they're encoded.
+ * DelegationDecoder helps to decode them when they're queried from the chain.
+ * ***
+ * The DelegationDecoder methods transform a [[QueryResult]] into an object of a KILT type.
  * @module Delegation/DelegationDecoder
  * @preferred
  */
