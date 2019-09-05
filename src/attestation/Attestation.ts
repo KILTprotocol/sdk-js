28d3337acb59d66835095800f17aad12d83e44f2
maudnals <maud.nalpas@gmail.com>
2019-09-05 10:46:13 +0200

    docs: public methods for attestation

--- src/attestation/Attestation.ts
+++ src/attestation/Attestation.ts
@@ -1,4 +1,10 @@
 /**
+ * Attestation provides the KILT ecosystem with certifying [[Claim]] objects, which are then written on the [[Blockchain]].
+ * ***
+ *  Attestation issued by the Attester is sent to and stored with the claimer. We call these [[AttestedClaim]]s Credentials.
+ *
+ * Attestation objects are stored on a map within the [[Blockchain]], the claimHash used as a key and a tuple of [[CType]] hash, account, delegation-id and revoked.
+ * The Attester can [[revoke]] a [[Claim]].
  * @module Attestation
  */
 import TxStatus from '../blockchain/TxStatus'
@@ -11,24 +17,6 @@ import { revoke, query, store } from './Attestation.chain'
 const log = factory.getLogger('Attestation')
 
 export default class Attestation implements IAttestation {
-  public static async query(claimHash: string): Promise<Attestation | null> {
-    return query(claimHash)
-  }
-
-  public static async revoke(
-    claimHash: string,
-    identity: Identity
-  ): Promise<TxStatus> {
-    return revoke(claimHash, identity)
-  }
-
-  /**
-   * Creates a new instance of this Attestation class from the given interface.
-   */
-  public static fromObject(obj: IAttestation): Attestation {
-    const newAttestation: Attestation = Object.create(Attestation.prototype)
-    return Object.assign(newAttestation, obj)
-  }
 
   public claimHash: IAttestation['claimHash']
   public cTypeHash: IAttestation['cTypeHash']
@@ -48,6 +36,62 @@ export default class Attestation implements IAttestation {
     this.revoked = revoked
   }
 
+  public static async query(claimHash: string): Promise<Attestation | null> {
+    return query(claimHash)
+  }
+
+  public static async revoke(
+    claimHash: string,
+    identity: Identity
+  ): Promise<TxStatus> {
+    return revoke(claimHash, identity)
+  }
+
+  /**
+   * [STATIC] Creates a new instance of this Attestation class from the given interface.
+   * @param `obj`
+   * @returns `Attestation`
+   * @example
+   * ```javascript
+   * const attestationTuple = encodedQueryResult.toJSON();
+   * const attestationObj: IAttestation = {
+   *    claimHash,
+   *    cTypeHash: attestationTuple[0],
+   *    owner: attestationTuple[1],
+   *    delegationId: attestationTuple[2],
+   *    revoked: attestationTuple[3],
+   * };
+   * const attestation = Attestation.fromObject(attestation)
+   * ```
+   */
+  public static fromObject(obj: IAttestation): Attestation {
+    const newAttestation: Attestation = Object.create(Attestation.prototype)
+    return Object.assign(newAttestation, obj)
+  }
+
+  /**
+   * @description Stores the Attestation on chain. Async.
+   * @param identity Account used to store the attestation.
+   * @returns Promise containing the [[TxStatus]].
+   * @example Use [store] to store an attestation on chain, and to create an [[AttestedClaim]] upon success:
+   * ```javascript
+   * // connect to the blockchain
+   * Kilt.default.connect('wss://full-nodes.kilt.io:9944');
+   * 
+   * // store the attestation on chain
+   * attestation.store(attester).then(() => {
+   *    // attestation was successfully stored so we can create an AttestedClaim
+   *    const attestedClaim = new Kilt.AttestedClaim(requestForAttestation, attestation);
+   * }).catch(e => {
+   *    console.log(e);
+   * }).finally(() => {
+   *    // disconnect from the blockchain
+   *    Kilt.BlockchainApiConnection.getCached().then(blockchain => {
+   *      blockchain.api.disconnect()
+   *    });
+   * });
+   * ```
+   */
   public async store(identity: Identity): Promise<TxStatus> {
     return store(this, identity)
   }
@@ -56,10 +100,21 @@ export default class Attestation implements IAttestation {
     return revoke(this.claimHash, identity)
   }
 
+  /**
+   * Queries the chain and returns whether the attestation is valid. An attestation is valid if it exist on chain, has the correct owner, and is not revoked.
+   * @param `claimHash` The hash of the claim to check.
+   * @returns A promise containing the boolean `attestationValid`.
+   * @example 
+   * ```javascript
+   * attestation.verify().then(isVerified => {
+   *    console.log('isVerified', isVerified);
+   * });
+   * ```
+   */
   public async verify(claimHash: string = this.claimHash): Promise<boolean> {
-    // 1) Query attestations for claimHash
+    // Query attestation by claimHash
     const attestation: Attestation | null = await query(claimHash)
-    // 2) check attestation for being valied, having the correct owner and not being revoked
+    // Check if attestation is valid
     const attestationValid: boolean =
       attestation !== null &&
       attestation.owner === this.owner &&
