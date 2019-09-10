99d19c6dc77996f15bdb21e21032c537b9e8e887
dudleyneedham <dudleyneedham1993@gmail.com>
2019-09-10 14:22:24 +0200

    docs: removed the changes of the iden branch

--- src/identity/Identity.ts
+++ src/identity/Identity.ts
@@ -28,43 +28,11 @@ type BoxPublicKey =
 
 export default class Identity extends PublicIdentity {
   private static ADDITIONAL_ENTROPY_FOR_HASHING = new Uint8Array([1, 2, 3])
-  /**
-   * @description [STATIC] Generates Mnemonic phrase used to create identities from phrase seed.
-   * @returns `generate` randomly generated mnemonic phrase (Secret phrase)
-   * @example
-   * ```javascript
-   *
-   * Identity.generateMnemonic()
-   * // (Output) mnemonic: "coast ugly state lunch repeat step armed goose together pottery bind mention"
-   *
-   * ```
-   */
+
   public static generateMnemonic() {
     return generate()
   }
 
-  /**
-   * @description [STATIC] Builds an identity object from a mnemonic string.
-   * @param phraseArg [[BIP39]](https://www.npmjs.com/package/bip39) Mnemonic word phrase. (Secret phrase)
-   * @returns `Identity`
-   *
-   * @example
-   *
-   * ```javascript
-   * //Build a Mnemonic to use as an argument
-   *
-   * const mnemonic = Identity.generateMnemonic()
-   * // mnemonic: "coast ugly state lunch repeat step armed goose together pottery bind mention"
-   *
-   * const testIdentity = Identity.buildFromMnemonic(mnemonic)
-   * //  (Output) Identity {
-   * //             address: '5GwqmTBQHWi6M6Bjek2ppXLVMDVBHx6ND8rs9eh1PnCjZBkr',
-   * //             boxPublicKeyAsHex: '0x26b353bb20038ff5068f00dd1d2a7bf4899a77bd9f6cf33c5ba267800d225872',
-   * //             ...
-   * //            }
-   *
-   * ```
-   */
   public static buildFromMnemonic(phraseArg?: string) {
     let phrase = phraseArg
     if (phrase) {
@@ -84,80 +52,17 @@ export default class Identity extends PublicIdentity {
     return Identity.buildFromSeed(seed)
   }
 
-  /**
-   * @description [STATIC] Builds an Identity, generated from a seed string as a hex.
-   * @param seedArg as hex string. (Starting with 0x)
-   * @returns  `Identity`
-   * @example
-   *
-   * ```javascript
-   *
-   * // Using the Mnemonic below to generate an Identity.
-   *
-   * const mnemonic = "coast ugly state lunch repeat step armed goose together pottery bind mention";
-   *
-   * const testerIdentity = Kilt.Identity.buildFromMnemonic(mnemonic);
-   *
-   * const testerIdentityCheck = Kilt.Identity.buildFromSeed(tester.seed);
-   *
-   * // (Output) Address: '5HXfLqrqbKoKyi61YErwUrWEa1PWxikEojV7PCnLJgxrWd6W'
-   * //          seedAsHex: '0x6ce9fd060c70165c0fc8da25810d249106d5df100aa980e0d9a11409d6b35261'
-   *
-   * ```
-   */
   public static buildFromSeedString(seedArg: string) {
     const asU8a = hexToU8a(seedArg)
     return Identity.buildFromSeed(asU8a)
   }
-  /**
-   * @description [STATIC] Builds a new Identity, generated from a seed (Secret Seed).
-   * @param seed as a Unit 8 Array
-   * @returns `Identity`
-   * @example
-   *
-   * ```javascript
-   *
-   * // Using the Mnemonic below to generate an Identity.
-   *
-   * const mnemonic = "coast ugly state lunch repeat step armed goose together pottery bind mention";
-   *
-   * const testerIdentity = Kilt.Identity.buildFromMnemonic(mnemonic);
-   *
-   * // (Output) testerIdentity.seed = seed: Uint8Array [
-   * //           108, 233, 253,  6,  12, 112,  22,  92,
-   * //           15, 200, 218, 37, 129,  13,  36, 145,
-   * //           6, 213, 223, 16,  10, 169, 128, 224,
-   * //           217, 161,  20,  9, 214, 179,  82,  97
-   * //           ],
-   *
-   * const testerIdentityCheck = Kilt.Identity.buildFromSeed(tester.seed);
-   *
-   * // (Output) Address: '5HXfLqrqbKoKyi61YErwUrWEa1PWxikEojV7PCnLJgxrWd6W'
-   *
-   * ```
-   */
+
   public static buildFromSeed(seed: Uint8Array) {
     const keyring = new Keyring({ type: 'ed25519' })
     const keyringPair = keyring.addFromSeed(seed)
     return new Identity(seed, keyringPair)
   }
-  /**
-   * @description [STATIC] Builds a new Identity, generated from a uniform resource identifier (URIs).
-   * @param uri Standard identifiers (//Alice)
-   * @returns `Identity` built from an URI
-   * @example
-   * ```javascript
-   *
-   * const identityBob = Identity.buildFromURI('//Bob')
-   *
-   * // Provides an URI.
-   * // (output) URI of Bob: Identity {
-   * //           address: '5GoNkf6WdbxCFnPdAnYYQyCjAKPJgLNxXwPjwTh6DGg6gcWn',
-   * //           ...
-   * //          }
-   *
-   * ```
-   */
+
   public static buildFromURI(uri: string) {
     const keyring = new Keyring({ type: 'ed25519' })
     const derived = keyring.createFromUri(uri)
@@ -202,20 +107,6 @@ export default class Identity extends PublicIdentity {
     return Crypto.sign(cryptoInput, this.signKeyringPair)
   }
 
-  /**
-   * @description Signs for a message string with an Identity.
-   * @param cryptoInput The 'CryptoInput' is raw data hashed.
-   * @returns `Crypto.signStr(...)`
-   * @example
-   * ```javascript
-   *
-   * const alice = Identity.buildFromMnemonic()
-   * // The identity is used to create a signature.
-   *
-   * alice.signStr()
-   * // (output) 0x0327f479bb8a6914...afc68c3ab0bf4e8de004
-   * ```
-   */
   public signStr(cryptoInput: CryptoInput) {
     return Crypto.signStr(cryptoInput, this.signKeyringPair)
   }
