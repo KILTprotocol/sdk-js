fa91c0ae64e7cfcb43c27d10e42ff32bfbf8cd89
dudleyneedham <dudleyneedham1993@gmail.com>
2019-10-09 18:26:14 +0200

    refactor: removed ctype utils to the demo client

--- src/ctype/CTypeUtils.ts
+++ src/ctype/CTypeUtils.ts
@@ -2,9 +2,7 @@
  * @module CType
  */
 import Ajv from 'ajv'
-
-import { CTypeModel, CTypeInputModel } from './CTypeSchema'
-import CType from './CType'
+import { CTypeModel } from './CTypeSchema'
 import ICType from '../types/CType'
 import Crypto from '../crypto'
 
@@ -39,104 +37,4 @@ export function verifyClaimStructure(claim: any, schema: any): boolean {
 
 export function getHashForSchema(schema: ICType['schema']): string {
   return Crypto.hashObjectAsStr(schema)
-}
-
-/**
- * Create the CTYPE model from a CTYPE input model (used in CTYPE editing components).
- * This is necessary because component editors rely on editing arrays of properties instead of
- * arbitrary properties of an object. Additionally the default language translations are integrated
- * into the input model and need to be separated for the CTYPE model.
- * This is the reverse function of CType.getCTypeInputModel(...).
- * @returns The CTYPE for the input model.
- */
-export function fromInputModel(ctypeInput: any): CType {
-  if (!verifySchema(ctypeInput, CTypeInputModel)) {
-    throw new Error('CType input does not correspond to input model schema')
-  }
-  const ctype = {
-    schema: {
-      $id: ctypeInput.$id,
-      $schema: CTypeModel.properties.$schema.default,
-      properties: {},
-      type: 'object',
-    },
-    metadata: {
-      title: {
-        default: ctypeInput.title,
-      },
-      description: {
-        default: ctypeInput.description,
-      },
-      properties: {},
-    },
-  }
-
-  const properties = {}
-  ctypeInput.properties.forEach((p: any) => {
-    const { title, $id, ...rest } = p
-    properties[$id] = rest
-    ctype.metadata.properties[$id] = {
-      title: {
-        default: title,
-      },
-    }
-  })
-  ctype.schema.properties = properties
-  return new CType(ctype as ICType)
-}
-
-function getLocalized(o: any, lang?: string): any {
-  if (lang == null || !o[lang]) {
-    return o.default
-  }
-  return o[lang]
-}
-
-/**
- * Create the CTYPE input model for a CTYPE editing component form the CTYPE model.
- * This is necessary because component editors rely on editing arrays of properties instead of
- * arbitrary properties of an object. Additionally the default language translations are integrated
- * into the input model. This is the reverse function of CType.fromInputModel(...).
- * @returns The CTYPE input model.
- */
-export function getCTypeInputModel(ctype: CType): any {
-  // create clone
-  const result = JSON.parse(JSON.stringify(ctype.schema))
-  result.$schema = CTypeInputModel.$id
-  result.title = getLocalized(ctype.metadata.title)
-  result.description = getLocalized(ctype.metadata.description)
-  result.required = []
-  result.properties = []
-
-  Object.entries(ctype.schema.properties as object).forEach(([key, value]) => {
-    result.properties.push({
-      title: getLocalized(ctype.metadata.properties[key].title),
-      $id: key,
-      type: value.type,
-    })
-    result.required.push(key)
-  })
-
-  return result
-}
-
-/**
- * This method creates an input model for a claim from a CTYPE.
- * It selects translations for a specific language from the localized part of the CTYPE meta data.
- * @param {string} lang the language to choose translations for
- * @returns {any} The claim input model
- */
-export function getClaimInputModel(ctype: ICType, lang?: string): any {
-  // create clone
-  const result = JSON.parse(JSON.stringify(ctype.schema))
-  result.title = getLocalized(ctype.metadata.title, lang)
-  result.description = getLocalized(ctype.metadata.description, lang)
-  result.required = []
-  Object.entries(ctype.metadata.properties as object).forEach(
-    ([key, value]) => {
-      result.properties[key].title = getLocalized(value.title, lang)
-      result.required.push(key)
-    }
-  )
-  return result
 }
