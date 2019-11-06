ff8c87feffeecfd23bd6f2817f48ce0aad5f21c6
dudleyneedham <dudleyneedham1993@gmail.com>
2019-11-06 16:04:41 +0100

    refactor: updated the ctypemetadata

--- src/ctype/CTypeMetadata.spec.ts
+++ src/ctype/CTypeMetadata.spec.ts
@@ -21,29 +21,21 @@ describe('CType', () => {
   } as ICType)
 
   const ctypeMetadata = {
-    metadata: {
-      $id: 'http://example.com/metadata-1',
-      $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-      title: { type: 'string' },
-      description: { type: 'string' },
-      properties: {
-        'first-property': { type: 'integer' },
-        'second-property': { type: 'string' },
-      },
-      type: 'object',
+    title: { type: 'string' },
+    description: { type: 'string' },
+    properties: {
+      'first-property': { type: 'integer' },
+      'second-property': { type: 'string' },
     },
-    ctypeHash: 'string',
-  } as ICTypeMetadata
+  } as ICTypeMetadata['metadata']
 
   const metadata = new CTypeMetadata(ctype, ctypeMetadata)
 
   it('verifies the metadata of a ctype', async () => {
     expect(metadata.ctypeHash).not.toHaveLength(0)
+    expect(CTypeUtils.verifySchema(metadata, CTypeWrapperMetadata)).toBeTruthy()
     expect(
       CTypeUtils.verifySchema(ctypeMetadata, CTypeWrapperMetadata)
-    ).toBeTruthy()
-    expect(
-      CTypeUtils.verifySchema(ctypeMetadata.metadata, CTypeWrapperMetadata)
     ).toBeFalsy()
   })
   it('checks if the metadata matches corresponding ctype hash', async () => {
