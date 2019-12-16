4346619d678c62d1032e28afee424952634fb987
Maud <maud.nalpas@gmail.com>
2019-12-16 16:42:06 +0100

    Feat: added signature/verification features to the DID Document… (#219)
    * feat: added signature and verification to Did doc
    
    * docs: documented new methods
    
    * test: wrote tests for did signatures
    
    * feat: removed unused method
    
    * refactor: cleared up Did implementation
    
    * feat: added extra security on signing
    
    * feat: changed verification function after review

--- src/did/Did.utils.ts
+++ src/did/Did.utils.ts
@@ -8,8 +8,20 @@
 import { isHex, hexToString } from '@polkadot/util'
 
 import IPublicIdentity from '../types/PublicIdentity'
+import Crypto from '../crypto'
+import Identity from '../identity/Identity'
 import { QueryResult } from '../blockchain/Blockchain'
-import Did, { IDid, IDENTIFIER_PREFIX } from './Did'
+import Did, {
+  IDid,
+  IDidDocument,
+  IDidDocumentSigned,
+  IDENTIFIER_PREFIX,
+  CONTEXT,
+  KEY_TYPE_AUTHENTICATION,
+  KEY_TYPE_SIGNATURE,
+  KEY_TYPE_ENCRYPTION,
+  SERVICE_KILT_MESSAGING,
+} from './Did'
 
 export function decodeDid(
   identifier: string,
@@ -44,4 +56,84 @@ export function getAddressFromIdentifier(
     throw new Error(`Not a KILT did: ${identifier}`)
   }
   return identifier.substr(IDENTIFIER_PREFIX.length)
+}
+
+export function createDefaultDidDocument(
+  identifier: string,
+  publicBoxKey: string,
+  publicSigningKey: string,
+  kiltServiceEndpoint?: string
+): IDidDocument {
+  return {
+    id: identifier,
+    '@context': CONTEXT,
+    authentication: {
+      type: KEY_TYPE_AUTHENTICATION,
+      publicKey: [`${identifier}#key-1`],
+    },
+    publicKey: [
+      {
+        id: `${identifier}#key-1`,
+        type: KEY_TYPE_SIGNATURE,
+        controller: identifier,
+        publicKeyHex: publicSigningKey,
+      },
+      {
+        id: `${identifier}#key-2`,
+        type: KEY_TYPE_ENCRYPTION,
+        controller: identifier,
+        publicKeyHex: publicBoxKey,
+      },
+    ],
+    service: kiltServiceEndpoint
+      ? [
+          {
+            type: SERVICE_KILT_MESSAGING,
+            serviceEndpoint: kiltServiceEndpoint,
+          },
+        ]
+      : [],
+  }
+}
+
+export function verifyDidDocumentSignature(
+  didDocument: IDidDocumentSigned,
+  identifier: string
+): boolean {
+  if (!didDocument || !didDocument.signature || !identifier) {
+    throw new Error(
+      `Missing data for verification (either didDocument, didDocumentHash, signature, or address is missing):\n
+          didDocument:\n
+          ${didDocument}\n
+          signature:\n
+          ${didDocument.signature}\n
+          address:\n
+          ${identifier}\n
+          `
+    )
+  }
+  const { id } = didDocument
+  if (identifier !== id) {
+    throw new Error(
+      `This identifier (${identifier}) doesn't match the DID Document's identifier (${id})`
+    )
+  }
+  const unsignedDidDocument = { ...didDocument }
+  delete unsignedDidDocument.signature
+  return Crypto.verify(
+    Crypto.hashObjectAsStr(unsignedDidDocument),
+    didDocument.signature,
+    getAddressFromIdentifier(identifier)
+  )
+}
+
+export function signDidDocument(
+  didDocument: IDidDocument,
+  identity: Identity
+): IDidDocumentSigned {
+  const didDocumentHash = Crypto.hashObjectAsStr(didDocument)
+  return {
+    ...didDocument,
+    signature: identity.signStr(didDocumentHash),
+  }
 }
