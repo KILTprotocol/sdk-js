73f1e793e51dbb0b08058d43b06a18954e290c06
LeonfLK <wenzel@galaniprojects.de>
2020-01-28 21:04:24 +0100

    feat: handle multiple transactions at once

--- src/blockchain/Blockchain.ts
+++ src/blockchain/Blockchain.ts
@@ -12,6 +12,7 @@ import { ApiPromise } from '@polkadot/api'
 import { SubmittableExtrinsic } from '@polkadot/api/promise/types'
 import { Header, Index } from '@polkadot/types/interfaces/types'
 import { Codec } from '@polkadot/types/types'
+import { UInt } from '@polkadot/types'
 import { ErrorHandler } from '../errorhandling/ErrorHandler'
 import { factory as LoggerFactory } from '../config/ConfigLog'
 import { ERROR_UNKNOWN, ExtrinsicError } from '../errorhandling/ExtrinsicError'
@@ -31,11 +32,10 @@ export type Stats = {
 
 export interface IBlockchainApi {
   api: ApiPromise
-
   getStats(): Promise<Stats>
   listenToBlocks(listener: (header: Header) => void): Promise<any> // TODO: change any to something meaningful
   submitTx(identity: Identity, tx: SubmittableExtrinsic): Promise<TxStatus>
-  getNonce(accountAddress: string): Promise<Codec>
+  getNonce(accountAddress: string): Promise<Index>
 }
 
 // Code taken from
@@ -52,12 +52,14 @@ export default class Blockchain implements IBlockchainApi {
   }
 
   public api: ApiPromise
-  public currentNonce: Index | null
+  public accountNonces: Map<Identity['address'], Index>
+  private pending = false
+  private nonceQueue: Array<(unlock: () => Promise<Index>) => void> = []
 
   public constructor(api: ApiPromise) {
     this.api = api
     this.errorHandler = new ErrorHandler(api)
-    this.currentNonce = null
+    this.accountNonces = new Map<Identity['address'], Index>()
   }
 
   private errorHandler: ErrorHandler
@@ -86,49 +88,97 @@ export default class Blockchain implements IBlockchainApi {
   ): Promise<TxStatus> {
     const accountAddress = identity.address
     const nonce = await this.getNonce(accountAddress)
-    const signed = identity.signSubmittableExtrinsic(tx, nonce.toHex())
-    log.info(`Submitting ${tx.method}`)
+    if (nonce === new UInt(-1)) {
+      throw new Error('Error while retrieving Nonce')
+    } else {
+      const signed = identity.signSubmittableExtrinsic(tx, nonce.toHex())
+      log.info(`Submitting ${tx.method} with Nonce ${nonce}`)
 
-    return new Promise<TxStatus>((resolve, reject) => {
-      signed
-        .send(result => {
-          log.info(`Got tx status '${result.status.type}'`)
+      return new Promise<TxStatus>((resolve, reject) => {
+        signed
+          .send(result => {
+            log.info(`Got tx status '${result.status.type}'`)
 
-          const { status } = result
-          if (ErrorHandler.extrinsicFailed(result)) {
-            log.warn(`Extrinsic execution failed`)
-            log.debug(`Transaction detail: ${JSON.stringify(result, null, 2)}`)
-            const extrinsicError: ExtrinsicError =
-              this.errorHandler.getExtrinsicError(result) || ERROR_UNKNOWN
+            const { status } = result
+            if (ErrorHandler.extrinsicFailed(result)) {
+              log.warn(`Extrinsic execution failed`)
+              log.debug(
+                `Transaction detail: ${JSON.stringify(result, null, 2)}`
+              )
+              const extrinsicError: ExtrinsicError =
+                this.errorHandler.getExtrinsicError(result) || ERROR_UNKNOWN
 
-            log.warn(`Extrinsic error ocurred: ${extrinsicError}`)
-            reject(extrinsicError)
-          }
-          if (status.type === FINALIZED) {
-            resolve(new TxStatus(status.type))
-          } else if (status.type === INVALID || status.type === DROPPED) {
-            reject(new Error(`Transaction failed with status '${status.type}'`))
-          }
-        })
-        .catch((err: Error) => {
-          // just reject with the original tx error from the chain
-          reject(err)
-        })
-    })
+              log.warn(`Extrinsic error ocurred: ${extrinsicError}`)
+              reject(extrinsicError)
+            }
+            if (status.type === 'Finalized') {
+              resolve(new TxStatus(status.type))
+            } else if (status.type === 'Invalid' || status.type === 'Dropped') {
+              reject(
+                new Error(`Transaction failed with status '${status.type}'`)
+              )
+            }
+          })
+          .catch((err: Error) => {
+            // just reject with the original tx error from the chain
+            reject(err)
+          })
+      })
+    }
   }
 
-  public async getNonce(accountAddress: string): Promise<Codec> {
-    if (!this.currentNonce) {
-      this.currentNonce = await this.api.query.system.accountNonce<Index>(
-        accountAddress
-      )
-      if (!this.currentNonce) {
-        throw Error(`Nonce not found for account ${accountAddress}`)
+  public async getNonce(accountAddress: string): Promise<Index> {
+    const unlock: () => Promise<Index> = await this.lock(accountAddress)
+    const nonce: Index = await unlock()
+    return nonce
+  }
+
+  private handleQueue(accountAddress: Identity['address']): void {
+    if (this.nonceQueue.length > 0) {
+      this.pending = true
+      const queuedPromise = this.nonceQueue.shift()
+      if (queuedPromise) {
+        queuedPromise(async () => {
+          let nonce: Index
+          try {
+            if (!this.accountNonces.has(accountAddress)) {
+              nonce = await this.api.query.system.accountNonce<Index>(
+                accountAddress
+              )
+              this.accountNonces.set(accountAddress, new UInt(nonce.addn(1)))
+            } else {
+              const temp = this.accountNonces.get(accountAddress)
+              if (!temp) {
+                throw new Error(
+                  `Nonce Retrieval Failed for : ${accountAddress}`
+                )
+              } else {
+                nonce = temp
+                this.accountNonces.set(accountAddress, new UInt(temp.addn(1)))
+              }
+            }
+          } catch (error) {
+            log.error(error)
+            nonce = new UInt(-1)
+          }
+          this.handleQueue(accountAddress)
+          return nonce
+        })
       }
     } else {
-      this.currentNonce.addn(1)
+      this.pending = false
     }
+  }
 
-    return this.currentNonce
+  private lock(
+    accountAddress: Identity['address']
+  ): Promise<() => Promise<Index>> {
+    const lock = new Promise<() => Promise<Index>>(resolve =>
+      this.nonceQueue.push(resolve)
+    )
+    if (!this.pending) {
+      this.handleQueue(accountAddress)
+    }
+    return lock
   }
 }
