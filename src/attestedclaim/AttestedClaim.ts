db6e6a9379f1045f696dd44e57e498fece9ad35d
LeonfLK <wenzel@galaniprojects.de>
2020-01-29 17:05:42 +0100

    chore: outsourced error throwing constructor inputchecks

--- src/attestedclaim/AttestedClaim.ts
+++ src/attestedclaim/AttestedClaim.ts
@@ -204,134 +204,17 @@ export default class AttestedClaim implements IAttestedClaim {
     return AttestedClaim.fromAttestedClaim(decompressedAttestedClaim)
   }
 
-  // private static constructorInputCheck(
-  //   attestedClaimInput: IAttestedClaim
-  // ): void {
-  //   const blake2bPattern = new RegExp('(0x)[A-F0-9]{64}', 'i')
-  //   if (!attestedClaimInput.request || !attestedClaimInput.attestation) {
-  //     throw new Error(
-  //       `Property Not Provided while building AttestedClaim!\n
-  //       attestedClaimInput.request: \n
-  //       ${attestedClaimInput.request} \n
-  //       attestedClaimInput.attestation: \n
-  //       ${attestedClaimInput.attestation}`
-  //     )
-  //   }
-  //   if (
-  //     !attestedClaimInput.attestation.cTypeHash ||
-  //     !attestedClaimInput.attestation.claimHash ||
-  //     !attestedClaimInput.attestation.owner
-  //   ) {
-  //     throw new Error(
-  //       `Property Not Provided while building Attestation!\n
-  //       attestationInput.cTypeHash:\n
-  //       ${attestedClaimInput.attestation.cTypeHash}\n
-  //       attestationInput.claimHash:\n
-  //       ${attestedClaimInput.attestation.claimHash}\n
-  //       attestationInput.owner:\n
-  //       ${attestedClaimInput.attestation.owner}`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.attestation.claimHash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided claimHash malformed:\n
-  //       ${attestedClaimInput.attestation.claimHash}`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.attestation.cTypeHash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided claimHash malformed:\n
-  //       ${attestedClaimInput.attestation.cTypeHash}`
-  //     )
-  //   }
-  //   if (!checkAddress(attestedClaimInput.attestation.owner, 42)[0]) {
-  //     throw new Error(`Owner address provided invalid`)
-  //   }
-  //   if (
-  //     !attestedClaimInput.request.claim ||
-  //     !attestedClaimInput.request.legitimations ||
-  //     !attestedClaimInput.request.claimOwner ||
-  //     !attestedClaimInput.request.claimerSignature ||
-  //     !attestedClaimInput.request.claimHashTree ||
-  //     !attestedClaimInput.request.cTypeHash ||
-  //     !attestedClaimInput.request.rootHash
-  //   ) {
-  //     throw new Error(
-  //       `Property Not Provided while building RequestForAttestation:\n
-  //         requestInput.claim:\n
-  //         ${attestedClaimInput.request.claim}\n
-  //         requestInput.legitimations:\n
-  //         ${attestedClaimInput.request.legitimations}\n
-  //         requestInput.claimOwner:\n
-  //         ${attestedClaimInput.request.claimOwner}\n
-  //         requestInput.claimerSignature:\n
-  //         ${attestedClaimInput.request.claimerSignature}
-  //         requestInput.claimHashTree:\n
-  //         ${attestedClaimInput.request.claimHashTree}\n
-  //         requestInput.rootHash:\n
-  //         ${attestedClaimInput.request.rootHash}\n
-  //         requestInput.cTypeHash:\n
-  //         ${attestedClaimInput.request.cTypeHash}\n`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.request.cTypeHash.hash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided cTypeHash malformed:\n
-  //       ${attestedClaimInput.request.cTypeHash.hash}\n
-  //       with Nonce: ${attestedClaimInput.request.cTypeHash.nonce}\n`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.request.rootHash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided cTypeHash malformed:\n
-  //       ${attestedClaimInput.request.rootHash}\n`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.request.claimOwner.hash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided cTypeHash malformed:\n
-  //       ${attestedClaimInput.request.rootHash}\n`
-  //     )
-  //   }
-  //   if (
-  //     !verify(
-  //       attestedClaimInput.request.rootHash,
-  //       attestedClaimInput.request.claimerSignature,
-  //       attestedClaimInput.request.claim.owner
-  //     )
-  //   ) {
-  //     throw new Error(`Provided claimer signature invalid`)
-  //   }
-  //   if (
-  //     !attestedClaimInput.request.claim.cTypeHash ||
-  //     !attestedClaimInput.request.claim.contents ||
-  //     !attestedClaimInput.request.claim.owner
-  //   ) {
-  //     throw new Error(
-  //       `Property Not Provided while building Claim:\n
-  //         claimInput.cTypeHash:\n
-  //           ${attestedClaimInput.request.claim.cTypeHash}\n
-  //           claimInput.contents:\n
-  //           ${attestedClaimInput.request.claim.contents}\n
-  //           claimInput.owner:\n'
-  //           ${attestedClaimInput.request.claim.owner}`
-  //     )
-  //   }
-  //   if (!attestedClaimInput.request.claim.cTypeHash.match(blake2bPattern)) {
-  //     throw new Error(
-  //       `Provided claimHash malformed:\n
-  //         ${attestedClaimInput.request.claim.cTypeHash}`
-  //     )
-  //   }
-  //   if (!checkAddress(attestedClaimInput.request.claim.owner, 42)[0]) {
-  //     throw new Error(`Owner address provided invalid`)
-  //   }
-  //   attestedClaimInput.request.legitimations.forEach(
-  //     (legitimation: IAttestedClaim) => {
-  //       if (!AttestedClaim.verify(legitimation)) {
-  //         throw new Error('')
-  //       }
-  //     }
-  //   )
-  // }
+  private static constructorInputCheck(
+    attestedClaimInput: IAttestedClaim
+  ): void {
+    if (!attestedClaimInput.request || !attestedClaimInput.attestation) {
+      throw new Error(
+        `Property Not Provided while building AttestedClaim!\n
+        attestedClaimInput.request: \n
+        ${attestedClaimInput.request} \n
+        attestedClaimInput.attestation: \n
+        ${attestedClaimInput.attestation}`
+      )
+    }
+  }
 }
