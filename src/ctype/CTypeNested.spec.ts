af6484f65ca8fb7256193815e62f684af8c76497
Timo Welde <tjwelde@gmail.com>
2020-02-13 12:09:27 +0100

    test: use $id of correct format

--- src/ctype/CTypeNested.spec.ts
+++ src/ctype/CTypeNested.spec.ts
@@ -8,7 +8,7 @@ describe('Nested CTypes', () => {
   const identityAlice = Identity.buildFromURI('//Alice')
 
   const passportCType: ICType['schema'] = {
-    $id: 'http://example.com/ctype-1',
+    $id: 'kilt:ctype:ctype-1',
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
     title: 'Passport',
     properties: {
@@ -22,7 +22,7 @@ describe('Nested CTypes', () => {
   }
 
   const kycCType: ICType['schema'] = {
-    $id: 'http://example.com/ctype-1',
+    $id: 'kilt:ctype:ctype-2',
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
     title: 'KYC',
     properties: {
@@ -76,7 +76,7 @@ describe('Nested CTypes', () => {
   }
 
   const nested: ICType['schema'] = {
-    $id: 'http://example.com/ctype-1',
+    $id: 'kilt:ctype:ctype-3',
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
     title: 'KYC and Passport',
     type: 'object',
@@ -108,7 +108,7 @@ describe('Nested CTypes', () => {
     },
   }
   const nestedDeeply: ICType['schema'] = {
-    $id: 'http://example.com/ctype-1',
+    $id: 'kilt:ctype:ctype-4',
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
     title: 'test',
     type: 'object',
