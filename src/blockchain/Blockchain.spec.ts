f2b984f8720c941b0e9ab3335dd4e3e1465b756a
LeonfLK <wenzel@galaniprojects.de>
2020-02-21 15:15:09 +0100

    feat: delete map entry after queue completion

--- src/blockchain/Blockchain.spec.ts
+++ src/blockchain/Blockchain.spec.ts
@@ -97,6 +97,18 @@ describe('queries', () => {
       expect(value.toNumber()).toEqual(new UInt(index).toNumber())
     })
   })
+
+  it('should delete map entry after queue is done', async () => {
+    const alice = Identity.buildFromURI('//Alice')
+    const alicePromisedNonces: Array<Promise<Index>> = []
+    const chain = new Blockchain(mockedApi)
+    for (let i = 0; i < 12; i += 1) {
+      alicePromisedNonces.push(chain.getNonce(alice.address))
+    }
+    Promise.all(alicePromisedNonces).then(v => {
+      expect(!chain.accountNonces.has(alice.address)).toBeTruthy()
+    })
+  })
 })
 const errorSetupApi = ({
   query: {
