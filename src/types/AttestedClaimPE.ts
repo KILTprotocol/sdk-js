4ef29518dcb3a372d7ef53651c289c369d681d24
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-02-26 17:55:34 +0100

    chore:  cleanup and add simple verification

--- src/types/AttestedClaimPE.ts
+++
@@ -1,6 +0,0 @@
-import IAttestedClaim from './AttestedClaim'
-import IAttestationPE from './AttestationPE'
-
-export default interface IAttestedClaimPE extends IAttestedClaim {
-  attestationPE: IAttestationPE
-}
