1505c81b9f1af5e037d9bc855b98181831641d26
wischli <w.freude@icloud.com>
2020-04-09 09:17:30 +0200

    tests: minor teardown improvement

--- src/__integrationtests__/jest.teardown.js
+++ src/__integrationtests__/jest.teardown.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getCached } = require('../blockchainApiConnection')
 
-module.exports = async function teardown() {
-  getCached()
+module.exports = function teardown() {
+  return getCached()
     .then((bc) => bc.api.disconnect())
     .finally(process.exit())
 }
