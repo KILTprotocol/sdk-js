cb58b97624dbd5b5369f1fd1a7741a76942d292d
wischli <w.freude@icloud.com>
2020-04-17 16:36:07 +0200

    refactor: propose test improvements

--- src/blockchain/Mocktest.spec.ts
+++ src/blockchain/Mocktest.spec.ts
@@ -1,5 +1,6 @@
 import BN from 'bn.js'
 import { Tuple, Option, U8a, Text } from '@polkadot/types'
+import { stringToHex } from '@polkadot/util'
 import { Identity } from '..'
 import { makeTransfer } from '../balance/Balance.chain'
 import getCached from '../blockchainApiConnection/BlockchainApiConnection'
@@ -8,24 +9,30 @@ import { queryByAddress } from '../did/Did.chain'
 jest.mock('../blockchainApiConnection/BlockchainApiConnection')
 
 describe('tx mocks', () => {
+  const alice = Identity.buildFromURI('//Alice')
+  const amount = new BN(1000)
   it('tx succeeds', async () => {
     require('../blockchainApiConnection/BlockchainApiConnection').__setDefaultResult(
       true
     )
-    const alice = Identity.buildFromURI('//Alice')
-    const amount = new BN(1000)
     const blockchain = await getCached()
     const transfer = blockchain.api.tx.balances.transfer(alice.address, amount)
     const result = await blockchain.submitTx(alice, transfer)
     expect(result).toMatchObject({ isFinalized: true })
+  })
+  it('tx fails', async () => {
+    require('../blockchainApiConnection/BlockchainApiConnection').__setDefaultResult(
+      false
+    )
+    await expect(makeTransfer(alice, alice.address, amount)).rejects.toThrow(
+      'Transaction failed'
+    )
   })
 
   it('tx succeeds then fails', async () => {
     require('../blockchainApiConnection/BlockchainApiConnection').__queueResults(
       [true, false]
     )
-    const alice = Identity.buildFromURI('//Alice')
-    const amount = new BN(1000)
     await expect(
       makeTransfer(alice, alice.address, amount)
     ).resolves.toMatchObject({
@@ -38,30 +45,41 @@ describe('tx mocks', () => {
 })
 
 describe('mock query result', () => {
-  beforeAll(async () => {
-    require('../blockchainApiConnection/BlockchainApiConnection').__mocked_api.query.did.dIDs.mockReturnValue(
-      new Option(
-        Tuple,
-        new Tuple(
-          // (publicBoxKey, publicSigningKey, documentStore?)
-          [Text, Text, U8a],
-          ['0x987', '0x123', '0x687474703a2f2f6d794449442e6b696c742e696f']
+  describe('DID', () => {
+    const documentStore = 'http://myDID.kilt.io'
+    const identifier = 'did:kilt:0xwertzui'
+
+    it('works for default DID mock', async () => {
+      const [publicBoxKey, publicSigningKey] = [
+        'publicBoxKey',
+        'publicSigningKey',
+      ]
+      await getCached()
+      await expect(queryByAddress('0xwertzui')).resolves.toMatchObject({
+        documentStore,
+        identifier,
+        publicBoxKey,
+        publicSigningKey,
+      })
+    })
+    it('works for custom DID mock', async () => {
+      const [publicBoxKey, publicSigningKey] = ['0x123', '0x321']
+      require('../blockchainApiConnection/BlockchainApiConnection').__mocked_api.query.did.dIDs.mockReturnValue(
+        new Option(
+          Tuple,
+          new Tuple(
+            // (publicSigningKey, publicBoxKey, documentStore?)
+            [Text, Text, U8a],
+            [publicSigningKey, publicBoxKey, stringToHex(documentStore)]
+          )
         )
       )
-    )
-    // would also work:
-    // ```ts
-    // const bc: any = await getCached()
-    // bc.api.query.did.dIDs.mockReturnValue(...)
-    // ```
-  })
-
-  it('works', async () => {
-    await expect(queryByAddress('0xwertzui')).resolves.toMatchObject({
-      documentStore: 'http://myDID.kilt.io',
-      identifier: 'did:kilt:0xwertzui',
-      publicBoxKey: '0x123',
-      publicSigningKey: '0x987',
+      await expect(queryByAddress('0xwertzui')).resolves.toMatchObject({
+        documentStore,
+        identifier,
+        publicBoxKey,
+        publicSigningKey,
+      })
     })
   })
 })
