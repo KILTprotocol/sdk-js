fafc62b2eedf247356a99f5b55a0555341da0b34
wischli <w.freude@icloud.com>
2020-05-20 09:54:23 +0200

    docs: fix minor lang error

--- src/identity/Identity.ts
+++ src/identity/Identity.ts
@@ -341,7 +341,7 @@ export default class Identity extends PublicIdentity {
    * const alice = Identity.buildFromMnemonic('car dog ...');
    * const tx = await blockchain.api.tx.ctype.add(ctype.hash);
    * const nonce = await blockchain.api.query.system.accountNonce(alice.address);
-   * alice.signSubmittableExtrinsic(tx, nonce.tohex());
+   * alice.signSubmittableExtrinsic(tx, nonce.toHex());
    * ```
    */
   public signSubmittableExtrinsic(
