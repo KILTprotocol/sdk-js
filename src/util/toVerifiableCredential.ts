1c3e0228459ea7cacfe984b81a8a6bcab49840c1
Raphael Flechtner <flechtner@galaniprojects.de>
2020-10-21 14:30:38 +0200

    chore: cleanup and documentation

--- src/util/toVerifiableCredential.ts
+++ src/util/toVerifiableCredential.ts
@@ -1,9 +1,12 @@
 import { decodeAddress } from '@polkadot/keyring'
 import { hexToU8a, u8aConcat, u8aToHex } from '@polkadot/util'
 import { signatureVerify } from '@polkadot/util-crypto'
+import CType from '../ctype'
+import { ERROR_CLAIM_CONTENTS_MALFORMED } from '../errorhandling/SDKErrors'
 import { IDidDocumentPublicKey } from '../did/Did'
-import { IAttestedClaim, Did, IPartialClaim, Attestation } from '..'
+import { IAttestedClaim, Did, Attestation, IClaim } from '..'
 import { hash } from '../crypto'
+import IRequestForAttestation from '../types/RequestForAttestation'
 
 /**
  * Constant for default context.
@@ -21,7 +24,7 @@ const DEFAULT_VERIFIABLECREDENTIAL_TYPE = 'VerifiableCredential'
 
 const KILT_SELF_SIGNED_PROOF_TYPE = 'KILTSelfSigned2020'
 const KILT_ATTESTED_PROOF_TYPE = 'KILTAttestation2020'
-const KILT_MATCH_PROPERTY_TYPE = 'KILTMatchProperty2020'
+const KILT_REVEAL_PROPERTY_TYPE = 'KILTRevealProperties2020'
 
 const KILT_STATUS_TYPE = 'KILTProtocolStatus2020'
 
@@ -47,25 +50,22 @@ interface selfSignedProof extends proof {
 
 interface attestedProof extends proof {
   type: typeof KILT_ATTESTED_PROOF_TYPE
-  credentialId: string | JSONreference
   attesterAddress: string | JSONreference
   delegationId?: string | JSONreference
 }
 
-interface matchPropertyProof extends proof {
-  type: typeof KILT_MATCH_PROPERTY_TYPE
+interface revealPropertyProof extends proof {
+  type: typeof KILT_REVEAL_PROPERTY_TYPE
   protected: {
-    claim: IPartialClaim
+    claim: Partial<IClaim>
   }
   nonces: {
-    claim: IPartialClaim
+    claim: Partial<IClaim>
   }
 }
 
 interface KILTcredentialStatus {
   type: typeof KILT_STATUS_TYPE
-  credentialId: string | JSONreference
-  attesterAddress: string
 }
 
 interface VerifiableCredential {
@@ -78,11 +78,14 @@ interface VerifiableCredential {
   // when the credential was issued
   issuanceDate: string
   // claims about the subjects of the credential
-  credentialSubject: Record<string, unknown>
+  credentialSubject: {
+    protected: Record<string, unknown>
+    id?: string
+  }
   // digital proof that makes the credential tamper-evident
   proof: proof | proof[]
   nonTransferable?: boolean
-  credentialStatus: KILTcredentialStatus
+  credentialStatus?: KILTcredentialStatus
   expirationDate?: any
 }
 
@@ -136,46 +139,18 @@ export default function attClaimToVC(
     signature: claimerSignature,
   } as selfSignedProof)
 
-  // fill in property values
-  const contentNonces: Record<string, string> = {}
-  Object.keys(claimHashTree).forEach((key) => {
-    const { nonce } = claimHashTree[key]
-    if (nonce) {
-      contentNonces[key] = nonce
-    }
-  })
-
-  proof.push({
-    type: KILT_MATCH_PROPERTY_TYPE,
-    protected: { claim },
-    nonces: {
-      claim: {
-        cTypeHash: cTypeHash.nonce,
-        contents: contentNonces,
-        owner: claimOwner.nonce,
-      },
-    },
-  } as matchPropertyProof)
-
   // add attestation proof
   const attester = input.attestation.owner
   const issuer = Did.getIdentifierFromAddress(attester)
   proof.push({
     type: KILT_ATTESTED_PROOF_TYPE,
     attesterAddress: attester,
-    credentialId: { $ref: '#/id' },
-    delegationId: delegationId
-      ? { $ref: '#/credentialSubject/protected/delegationId' }
-      : undefined,
   } as attestedProof)
 
-  const credentialStatus: KILTcredentialStatus = {
-    type: KILT_STATUS_TYPE,
-    attesterAddress: attester,
-    credentialId: { $ref: '#/id' },
-  }
-
+  // add current date bc we have no issuance date on credential
+  // TODO: could we get this from block time or something?
   const issuanceDate = new Date().toISOString()
+
   return {
     '@context': [DEFAULT_VERIFIABLECREDENTIAL_CONTEXT],
     type: [DEFAULT_VERIFIABLECREDENTIAL_TYPE],
@@ -185,7 +160,45 @@ export default function attClaimToVC(
     proof,
     issuer,
     issuanceDate,
-    credentialStatus,
+  }
+}
+
+/**
+ * This proof can be added to a credential to reveal selected properties to the verifier.
+ * For each property to be revealed, it also contains a nonce which is required to verify against the hash in the credential.
+ * Values, nonces and hashes are mapped to each through via identical data structures.
+ *
+ * @param partialClaim Claim object containing only the values you want to reveal.
+ * @param requestForAttestation The full [[IRequestForAttestation]] object from which necessary nonces are picked.
+ * @returns Proof object that can be included in a Verifiable Credential / Verifiable Presentation's proof section.
+ */
+export function makeRevealPropertiesProof(
+  partialClaim: Partial<IClaim>,
+  requestForAttestation: IRequestForAttestation
+): revealPropertyProof {
+  const { claimHashTree, cTypeHash, claimOwner } = requestForAttestation
+  // pull nonce for each property in claim contents that has not been removed or nullified
+  const contentNonces: Record<string, string> = {}
+  if (partialClaim.contents) {
+    Object.entries(partialClaim.contents).forEach(([key, value]) => {
+      const { nonce } = claimHashTree[key]
+      if (nonce && typeof value !== 'undefined' && value !== null) {
+        contentNonces[key] = nonce
+      }
+    })
+  }
+  // compile object with structure identical to IClaim object
+  const claimNonces: Partial<IClaim> = {
+    contents: contentNonces,
+  }
+  // add nonces for cTypeHash & owner if not removed/nullified in claim
+  if (partialClaim.cTypeHash) claimNonces.cTypeHash = cTypeHash.nonce
+  if (partialClaim.owner) claimNonces.owner = claimOwner.nonce
+  // return the proof containing values and nonces in two objects of identical structure
+  return {
+    type: KILT_REVEAL_PROPERTY_TYPE,
+    protected: { claim: partialClaim },
+    nonces: { claim: claimNonces },
   }
 }
 
@@ -200,6 +213,16 @@ const CREDENTIAL_MALFORMED_ERROR = (reason: string): Error =>
 const PROOF_MALFORMED_ERROR = (reason: string): Error =>
   new Error(`Proof malformed: ${reason}`)
 
+/**
+ * Verifies a KILT self signed proof (claimer signature) against a KILT style Verifiable Credential.
+ * This entails computing the root hash from the hashes contained in the `protected` section of the credentialSubject.
+ * The resulting hash is then verified against the signature and public key contained in the proof (the latter
+ * could be a DID reference in the future). It is also expected to by identical to the credential id.
+ *
+ * @param credential Verifiable Credential to verify proof against.
+ * @param proof KILT self signed proof object.
+ * @returns Object indicating whether proof could be verified.
+ */
 export function verifySelfSignedProof(
   credential: VerifiableCredential,
   proof: selfSignedProof
@@ -235,7 +258,7 @@ export function verifySelfSignedProof(
     while (queue.length) {
       // pop first element off array
       const first = queue.shift()
-      if (typeof first === 'object') {
+      if (typeof first === 'object' && first) {
         // if first element is object, retrieve values and push to BEGINNING of queue
         queue = Object.values(first).concat(...queue)
       } else if (typeof first === 'string') {
@@ -253,10 +276,15 @@ export function verifySelfSignedProof(
       ...hashes.map((hexHash) => hexToU8a(hexHash))
     )
     const rootHash = hash(concatenated)
+
+    // throw if root hash does not match expected (=id)
+    const expectedRootHash = credential.id
+    if (expectedRootHash !== u8aToHex(rootHash))
+      throw new Error('computed root hash does not match expected')
+
     // validate signature over root hash
-    if (!signatureVerify(rootHash, proof.signature, signerPubKey).isValid) {
+    if (!signatureVerify(rootHash, proof.signature, signerPubKey).isValid)
       throw new Error('signature could not be verified')
-    }
     return result
   } catch (e) {
     result.verified = false
@@ -265,6 +293,15 @@ export function verifySelfSignedProof(
   }
 }
 
+/**
+ * Verifies a KILT attestation proof by querying data from the KILT blockchain.
+ * This includes querying the KILT blockchain with the credential id, which returns an attestation record if attested.
+ * This record is then compared against attester address and delegation id (the latter of which is taken directly from the credential).
+ *
+ * @param credential Verifiable Credential to verify proof against.
+ * @param proof KILT self signed proof object.
+ * @returns Object indicating whether proof could be verified.
+ */
 export async function verifyAttestedProof(
   credential: VerifiableCredential,
   proof: attestedProof
@@ -274,47 +311,34 @@ export async function verifyAttestedProof(
     // check proof
     if (proof.type !== KILT_ATTESTED_PROOF_TYPE)
       throw new Error('Proof type mismatch')
-    let attesterAddress: string
-    if (typeof proof.attesterAddress === 'string') {
-      attesterAddress = proof.attesterAddress
-      // } else if (proof.attesterAddress === { $ref: '#/id' }) {
-      //   attesterAddress = credential.id
-    } else {
+    const { attesterAddress } = proof
+    if (typeof attesterAddress !== 'string' || !attesterAddress)
       throw PROOF_MALFORMED_ERROR('attester address not understood')
-    }
-    let claimHash: string
-    if (typeof proof.credentialId === 'string') {
-      claimHash = proof.credentialId
-    } else if (
-      typeof proof.credentialId === 'object' &&
-      proof.credentialId.$ref === '#/id'
-    ) {
-      claimHash = credential.id
-    } else {
-      throw PROOF_MALFORMED_ERROR('credentialId reference not understood')
-    }
+    const claimHash = credential.id
+    if (typeof claimHash !== 'string' || !claimHash)
+      throw CREDENTIAL_MALFORMED_ERROR(
+        'claim id (=claim hash) missing / invalid'
+      )
     let delegationId: string | null
-    if (typeof proof.delegationId === 'undefined') {
-      delegationId = null
-    } else if (typeof proof.delegationId === 'string') {
-      delegationId = proof.delegationId
-    } else if (
-      typeof proof.delegationId === 'object' &&
-      proof.delegationId.$ref ===
-        '#/credentialSubject/protected/delegationId' &&
-      typeof (credential.credentialSubject as any).protected.delegationId ===
-        'string'
-    ) {
-      delegationId = (credential.credentialSubject as any).protected
-        .delegationId
-    } else {
-      throw PROOF_MALFORMED_ERROR('credentialId reference not understood')
+
+    switch (typeof credential.credentialSubject.protected.delegationId) {
+      case 'string':
+        delegationId = credential.credentialSubject.protected.delegationId
+        break
+      case 'undefined':
+        delegationId = null
+        break
+      default:
+        throw CREDENTIAL_MALFORMED_ERROR('delegationId not understood')
     }
+    // query on-chain data by credential id (= claim root hash)
     const onChain = await Attestation.query(claimHash)
+    // if not found, credential has not been attested, proof is invalid
     if (!onChain)
       throw new Error(
         `attestation for credential with id ${claimHash} not found`
       )
+    // if data on proof does not correspond to data on chain, proof is incorrect
     if (
       onChain.owner !== attesterAddress ||
       onChain.delegationId !== delegationId
@@ -325,6 +349,7 @@ export async function verifyAttestedProof(
           delegation: delegationId,
         }}`
       )
+    // if proof data is valid but attestation is flagged as revoked, credential is no longer valid
     if (onChain.revoked) throw new Error('attestation revoked')
     return result
   } catch (e) {
@@ -334,14 +359,28 @@ export async function verifyAttestedProof(
   }
 }
 
-export function verifyPropertyProof(
+function getByPath(object: Record<string, any>, path: string[]): any | null {
+  return path.reduce((indexable, nextIndex) => indexable?.[nextIndex], object)
+}
+
+/**
+ * Verifies a proof that reveals the content of selected properties to a verifier. This enables selective disclosure.
+ * Values and nonces contained within this proof will be hashed, the result of which is expected to equal hashes on the credential.
+ *
+ * @param credential Verifiable Credential to verify proof against.
+ * @param proof KILT self signed proof object.
+ * @param schema If the CType for this credential is passed, the revealed properties will additionally be subjected to a schema validation.
+ * @returns Object indicating whether proof could be verified.
+ */
+export function verifyRevealPropertyProof(
   credential: VerifiableCredential,
-  proof: matchPropertyProof
+  proof: revealPropertyProof,
+  schema?: CType
 ): VerificationResult {
   const result: VerificationResult = { verified: true }
   try {
     // check proof
-    if (proof.type !== KILT_MATCH_PROPERTY_TYPE)
+    if (proof.type !== KILT_REVEAL_PROPERTY_TYPE)
       throw new Error('Proof type mismatch')
     if (
       typeof proof.nonces !== 'object' ||
@@ -355,11 +394,11 @@ export function verifyPropertyProof(
     if (typeof protectedBody !== 'object' || !protectedBody)
       throw CREDENTIAL_MALFORMED_ERROR('protected credential body missing')
 
-    const getNested = (
-      object: Record<string, any>,
-      path: string[]
-    ): any | null =>
-      path.reduce((indexable, nextIndex) => indexable?.[nextIndex], object)
+    // perform schema validation
+    if (schema && !schema.verifyClaimStructure(proof.protected.claim as IClaim))
+      throw ERROR_CLAIM_CONTENTS_MALFORMED()
+
+    // iteratively hash nonce + value and compare to hash in credential
     const verificationQueue: string[][] = Object.entries(
       proof.protected
     ).map(([key]) => [key])
@@ -368,15 +407,15 @@ export function verifyPropertyProof(
     while (verificationQueue.length) {
       // pop last element off array
       const path = verificationQueue.pop() || []
-      const value = getNested(proof.protected, path)
+      const value = getByPath(proof.protected, path)
       if (typeof value === 'object' && value) {
         // if first value is object, retrieve values and push to end of queue
         verificationQueue.push(
           ...Object.entries(value).map(([key]) => [...path, key])
         )
       } else {
-        const nonce = getNested(proof.nonces, path)
-        const hashInCredential = getNested(protectedBody, path)
+        const nonce = getByPath(proof.nonces, path)
+        const hashInCredential = getByPath(protectedBody, path)
         const pathAsString = path.reduce((last, next) => `${last}.${next}`)
         if (typeof nonce !== 'string')
           throw PROOF_MALFORMED_ERROR(
