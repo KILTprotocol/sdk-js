65f764f2e641908d68ee2502dcfc896aec6c9e0f
LeonfLK <wenzel@galaniprojects.de>
2021-01-27 17:45:52 +0100

    fix: develop index

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -3,4 +3,7 @@
  * @ignore
  */
 
-export * from './ConfigService'
+import * as ConfigService from './ConfigService'
+
+// eslint-disable-next-line import/prefer-default-export
+export { ConfigService }
