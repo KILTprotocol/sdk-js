1926ac1132281e1d42ab2b2bae027ed63ccb4910
Albrecht <14820950+weichweich@users.noreply.github.com>
2021-02-04 14:45:28 +0100

    Revert "test: remove tests that rely on timing"
    This reverts commit 4cfe10afd186d123bc744f28dc99b7a1bce256b8.

--- packages/core/src/__integrationtests__/Delegation.spec.ts
+++ packages/core/src/__integrationtests__/Delegation.spec.ts
@@ -8,7 +8,11 @@ import { cryptoWaitReady } from '@polkadot/util-crypto'
 import { Permission } from '@kiltprotocol/types'
 import { AttestedClaim, Identity } from '..'
 import Attestation from '../attestation/Attestation'
-import { IS_IN_BLOCK, submitTxWithReSign } from '../blockchain/Blockchain.utils'
+import {
+  IS_IN_BLOCK,
+  IS_READY,
+  submitTxWithReSign,
+} from '../blockchain/Blockchain.utils'
 import { config, disconnect } from '../kilt'
 import Claim from '../claim/Claim'
 import {
@@ -103,9 +107,7 @@ describe('when there is an account hierarchy', () => {
       HashSignedByDelegate = attester.signStr(delegatedNode.generateHash())
       await rootNode
         .store(uncleSam)
-        .then((tx) =>
-          submitTxWithReSign(tx, uncleSam, { resolveOn: IS_IN_BLOCK })
-        )
+        .then((tx) => submitTxWithReSign(tx, uncleSam, { resolveOn: IS_READY }))
       await delegatedNode.store(uncleSam, HashSignedByDelegate).then((tx) =>
         submitTxWithReSign(tx, uncleSam, {
           resolveOn: IS_IN_BLOCK,
