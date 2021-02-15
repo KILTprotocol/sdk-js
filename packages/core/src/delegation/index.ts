d65c369c6aafe72338f40ae896028ad9ed2921b7
dudleyneedham <dudleyneedham1993@gmail.com>
2021-02-15 17:45:21 +0100

    refactor: moved the steps tracker to delegation node utils

--- packages/core/src/delegation/index.ts
+++ packages/core/src/delegation/index.ts
@@ -6,5 +6,11 @@
 import DelegationBaseNode from './Delegation'
 import DelegationNode from './DelegationNode'
 import DelegationRootNode from './DelegationRootNode'
+import { checkTraversalStepsToParent } from './DelegationNode.utils'
 
-export { DelegationBaseNode, DelegationNode, DelegationRootNode }
+export {
+  DelegationBaseNode,
+  DelegationNode,
+  DelegationRootNode,
+  checkTraversalStepsToParent,
+}
