4d129f2867e9ab1400c08146aca057172108c7e9
Timo Welde <tjwelde@gmail.com>
2021-02-19 16:10:06 +0100

    feat: rename functions and add docs

--- packages/chain-helpers/src/blockchainApiConnection/BlockchainApiConnection.ts
+++ packages/chain-helpers/src/blockchainApiConnection/BlockchainApiConnection.ts
@@ -53,6 +53,12 @@ export const CUSTOM_TYPES: RegistryTypes = {
   },
 }
 
+/**
+ * Builds a new blockchain connection instance.
+ *
+ * @param host Optional host address. Otherwise taken from the ConfigService.
+ * @returns A new blockchain connection instance.
+ */
 export async function buildConnection(
   host: string = ConfigService.get('address')
 ): Promise<Blockchain> {
@@ -64,23 +70,61 @@ export async function buildConnection(
   return new Blockchain(api)
 }
 
-export async function getCached(
-  host: string = ConfigService.get('address')
-): Promise<Blockchain> {
+/**
+ * Allows caching of a self-built connection instance.
+ * This will be automatically used by all chain functions.
+ *
+ * For advanced use-cases only.
+ *
+ * @param connectionInstance The Blockchain instance, which should be cached.
+ */
+export function setConnection(connectionInstance: Blockchain): void {
+  instance = connectionInstance
+}
+
+/**
+ * Gets the cached blockchain connection instance.
+ *
+ * @returns Cached blockchain connection.
+ */
+export function getConnection(): Blockchain | null {
+  return instance
+}
+
+/**
+ * Gets the cached blockchain connection, or builds a new one, if non-existant.
+ *
+ * @returns The cached or newly built blockchain connection instance.
+ */
+export async function getConnectionOrConnect(): Promise<Blockchain> {
   if (!instance) {
-    instance = await buildConnection(host)
+    instance = await buildConnection()
   }
   return instance
 }
 
+/**
+ * Clears the cached blockchain connection instance.
+ * This does NOT disconnect automatically beforehand!
+ */
 export function clearCache(): void {
   instance = null
 }
 
+/**
+ * Check, if the cached blockchain connection is connected.
+ *
+ * @returns If there is a cached connection and it is currently connected.
+ */
 export async function connected(): Promise<boolean> {
   return !!instance && instance.api.isConnected
 }
 
+/**
+ * Disconnects the cached connection and clears the cache.
+ *
+ * @returns If there was a cached and connected connection, or not.
+ */
 export async function disconnect(): Promise<boolean> {
   const isConnected = await connected()
   if (isConnected) {
@@ -89,5 +133,3 @@ export async function disconnect(): Promise<boolean> {
   clearCache()
   return isConnected
 }
-
-export default getCached
