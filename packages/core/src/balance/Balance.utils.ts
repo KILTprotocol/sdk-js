218f44c8d80281cc5599cca8b702f8b9377a65a6
dudleyneedham <dudleyneedham1993@gmail.com>
2021-03-30 10:20:18 +0200

    feat: adding new decimal placement method

--- packages/core/src/balance/Balance.utils.ts
+++ packages/core/src/balance/Balance.utils.ts
@@ -8,12 +8,42 @@ import { formatBalance } from '@polkadot/util'
 
 export const KILT_COIN = new BN(1)
 
-export function formatKiltBalance(amount: BN): string {
-  return formatBalance(amount, {
+// Exported options from polkadot/util
+interface Options {
+  decimals?: number
+  forceUnit?: string
+  withSi?: boolean
+  withSiFull?: boolean
+  withUnit?: boolean | string
+}
+
+export function formatKiltBalance(
+  amount: BN,
+  additionalOptions?: Options
+): string {
+  const options = {
     decimals: 15,
     withSiFull: true,
     withUnit: 'KILT',
-  })
+    ...additionalOptions,
+  }
+  return formatBalance(amount, options)
+}
+
+export function formatKiltBalanceDecimalPlacement(
+  amount: BN,
+  decimal: number,
+  denomination: number
+): string {
+  const decimalBN = new BN(decimal)
+  const denominationBN = new BN(denomination)
+
+  const balanceFactoring = new BN(10).pow(denominationBN.sub(decimalBN))
+  const powersOfTen = new BN(10).pow(decimalBN).toNumber()
+  const amountFactoring = amount.div(balanceFactoring).toNumber()
+
+  const amountDenomination = amountFactoring / powersOfTen
+  return amountDenomination.toFixed(decimal)
 }
 
 export function convertToTxUnit(balance: BN, power: number): BN {
