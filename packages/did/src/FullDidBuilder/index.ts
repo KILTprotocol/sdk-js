de94741051bdb08e2a4d270f7e837850a171ead3
Antonio Antonino <antonio@kilt.io>
2021-12-09 10:19:41 +0100

    chore: add index.ts

---
+++ packages/did/src/FullDidBuilder/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2018-2021 BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+export * from './FullDidBuilder'
+export * from './FullDidCreationBuilder'
+export * from './FullDidUpdateBuilder'
---
+++ packages/did/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2018-2021 BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+export * from './DemoKeystore'
+export * from './DidDetails'
+export * from './DidDocumentExporter'
+export * from './DidResolver'
+export * from './FullDidBuilder'
+export * as DidChain from './Did.chain'
+export * as DidUtils from './Did.utils'
+
+export * from './types'
