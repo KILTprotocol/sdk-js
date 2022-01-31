0ef76b6175a6571e9e27c99fcbcad4f77542dfe1
dudleyneedham <dudleyneedham1993@gmail.com>
2022-01-31 16:08:27 +0100

    feat: adding the constants to the vc exporter

--- packages/vc-export/src/index.ts
+++ packages/vc-export/src/index.ts
@@ -7,6 +7,7 @@
 
 import type * as types from './types.js'
 
+export * as constants from './constants.js'
 export * as verification from './verificationUtils.js'
 export * as presentation from './presentationUtils.js'
 export { fromCredential } from './exportToVerifiableCredential.js'
