f72f1fc63b5744b7d0c0165618a5ef3924fb0474
dudleyneedham <dudleyneedham1993@gmail.com>
2022-02-02 17:11:51 +0100

    feat: naming constants export as constants

--- packages/vc-export/src/index.ts
+++ packages/vc-export/src/index.ts
@@ -6,7 +6,7 @@
  */
 
 export * from './types.js'
-export * from './constants.js'
+export * as constants from './constants.js'
 export * as verification from './verificationUtils.js'
 export * as presentation from './presentationUtils.js'
 export { fromCredential } from './exportToVerifiableCredential.js'
