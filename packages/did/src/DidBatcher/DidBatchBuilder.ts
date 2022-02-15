659271a31de910760809db62c4d2f82908036e63
Antonio Antonino <antonio@kilt.io>
2022-02-15 10:33:37 +0100

    docs: add docs for the DID batch builder

--- packages/did/src/DidBatcher/DidBatchBuilder.ts
+++ packages/did/src/DidBatcher/DidBatchBuilder.ts
@@ -31,13 +31,27 @@ type BatchInfo = {
   extrinsics: Extrinsic[]
 }
 
+/**
+ * Type of a closure used to select one of the key candidates for a DID to sign a given batch of extrinsics.
+ */
 export type BatchSigningKeySelection = (
   batch: Extrinsic[],
   keys: DidVerificationKey[]
 ) => Promise<DidVerificationKey>
+/**
+ * The default signign key selection closure which returns the first key in the list of key candidates.
+ *
+ * @param batch The batch of extrinsics to sign. All extrinsics are guaranteed to require the same signing key relationship.
+ * @param keys The list of key candidates that match the required key relationship for the given extrinsic batch.
+ *
+ * @returns The selected key among the list of key candidates. If the returned key is not in the input list of candidates, the signing operation or the extrinsic submission will most likely fail.
+ */
 export const defaultBatchSigningKeySelectionClosure: BatchSigningKeySelection =
   (batch: Extrinsic[], keys: DidVerificationKey[]) => Promise.resolve(keys[0])
 
+/**
+ * A builder to batch multiple DID-authorised extrinsics which are to be submitted by the same KILT account.
+ */
 export class DidBatchBuilder {
   private did: FullDidDetails
   private api: ApiPromise
@@ -45,6 +59,12 @@ export class DidBatchBuilder {
   protected batches: BatchInfo[] = []
   protected isConsumed = false
 
+  /**
+   * Create a new builder with the provided WS connection and [[FullDidDetails]].
+   *
+   * @param api The [[ApiPromise]] wrapping the WS connection to a KILT RPC node.
+   * @param did The [[FullDidDetails]] used to fetch the key material needed to authorise the batched operations.
+   */
   public constructor(api: ApiPromise, did: FullDidDetails) {
     this.did = did
     this.api = api
@@ -69,6 +89,20 @@ export class DidBatchBuilder {
     }
   }
 
+  /**
+   * Add a new extrinsic to the batch.
+   *
+   * If the new extrinsic requires a different key type than the previous one, a new batch is created internally.
+   * Order of all extrinsics is maintained throughtout the lifetime of the builder and when they are submitted to the KILT blockchain.
+   *
+   * The requirements to add a new extrinsic to the batch are the following:
+   * - The extrinsic must not be a DID management extrinsic. For those, [[FullDidCreationBuilder]] and [[FullDidUpdateBuilder]] must be used
+   * - The extrinsic must require a DID origin. E.g., staking extrinsics that require a simple KILT account origin cannot be added to the batch
+   * - The DID must have at least one key candidate to sign the provided extrinsic. If the DID is updated after the extrinsic is added but before the builder is consumed, it results in undefined behaviour, and most likely the extrinsic submission will fail.
+   *
+   * @param extrinsic The [[Extrinsic]] to add to the batch.
+   * @returns The builder containing the new extrinsic in the last position of the internal queue.
+   */
   public addSingleExtrinsic(extrinsic: Extrinsic): this {
     if (this.isConsumed) {
       throw SDKErrors.ERROR_DID_BUILDER_ERROR(
@@ -82,6 +116,15 @@ export class DidBatchBuilder {
     return this
   }
 
+  /**
+   * Add multiple extrinsics to the batch.
+   *
+   * The order of the extrinsics is maintained in respect to how they are provided in the function.
+   * All extrinsics are appended to the internal queue, so that the first in the provided list is added after the last in the builder's queue.
+   *
+   * @param extrinsics The list of [[Extrinsic]] to add to the batch.
+   * @returns The builder containing the new extrinsics.
+   */
   public addMultipleExtrinsics(extrinsics: Extrinsic[]): this {
     extrinsics.forEach((ext) => {
       this.addSingleExtrinsic(ext)
@@ -90,6 +133,18 @@ export class DidBatchBuilder {
     return this
   }
 
+  /**
+   * Consume the builder and generate the [[SubmittableExtrinsic]] containing the batch of extrinsics to execute, in the order they were added to the builder.
+   *
+   * @param signer The [[KeystoreSigner]] to sign the DID operation. It must contain the required keys to sign each batch.
+   * @param submitter The KILT address of the user authorised to submit each extrinsic in the batch.
+   * @param submissionOptions The additional options to customise the signing operation.
+   * @param submissionOptions.atomic A flag indicating whether the whole batch must be reverted (true) or not (false) in case any extrinsic in the batch fails. It defaults to true.
+   * @param submissionOptions.keySelection The [[BatchSigningKeySelection]] closure to specify the DID key to use for each batch. It defaults to [[defaultBatchSigningKeySelectionClosure]].
+   * @param submissionOptions.initialNonce The initial nonce to use for the first batch, after which the nonce is increased by the builder. It defaults to the next valid DID nonce as stored on chain at the time this function is called.
+   *
+   * @returns The [[SubmittableExtrinsic]] containing the batch of batches.
+   */
   public async consume(
     signer: KeystoreSigner,
     submitter: IIdentity['address'],
