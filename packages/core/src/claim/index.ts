rflechtner/kilt-sdk

feat(claim): rename files

packages/core/src/claim/index.ts
@@ -5,6 +5,6 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-export * from './Claim.js'
-export * from './Claim.compression.js'
-export * from './Claim.utils.js'
+export * from './claim.js'
+export * from './compression.js'
+export * from './utils.js'

KILTprotocol/sdk-js

packages/core/src/claim/index.ts
@@ -5,6 +5,6 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-export * from './Claim.js'
-export * from './Claim.compression.js'
-export * from './Claim.utils.js'
+export * from './base.js'
+export * from './compression.js'
+export * from './utils.js'