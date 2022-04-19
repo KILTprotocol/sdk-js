rflechtner/kilt-sdk

feat: simplify ctype

packages/core/src/ctype/index.ts
@@ -5,7 +5,8 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-export * as CType from './CType.js'
-export * as CTypeUtils from './CType.utils.js'
-export * as CTypeMetadata from './CTypeMetadata.js'
-export * as CTypeSchema from './CTypeSchema.js'
+export * from './ctype.js'
+export * from './utils.js'
+export * from './verification.js'
+export * from './compression.js'
+export * as Schemas from './schemas.js'

packages/core/src/ctype/metadata.spec.ts
@@ -13,10 +13,9 @@
 
 import { SDKErrors } from '@kiltprotocol/utils'
 import type { ICType, ICTypeMetadata } from '@kiltprotocol/types'
-import * as CType from './CType'
-import * as CTypeUtils from './CType.utils'
-import * as CTypeMetadata from './CTypeMetadata'
-import { MetadataModel } from './CTypeSchema'
+import * as CType from './ctype'
+import * as Verification from './verification'
+import { MetadataModel } from './schemas'
 
 describe('CType', () => {
   const didAlice = 'did:kilt:4p6K4tpdZtY3rNqM2uorQmsS6d3woxtnWMHjtzGftHmDb41N'
@@ -54,10 +53,14 @@ describe('CType', () => {
   })
 
   it('verifies the metadata of a ctype', async () => {
-    expect(() => CTypeMetadata.check(metadata)).not.toThrow()
+    expect(() => Verification.verifyCTypeMetadata(metadata)).not.toThrow()
     expect(metadata.ctypeHash).not.toHaveLength(0)
-    expect(CTypeUtils.verifySchema(metadata, MetadataModel)).toBeTruthy()
-    expect(CTypeUtils.verifySchema(ctypeMetadata, MetadataModel)).toBeFalsy()
+    expect(
+      Verification.verifyObjectAgainstSchema(metadata, MetadataModel)
+    ).toBeTruthy()
+    expect(
+      Verification.verifyObjectAgainstSchema(ctypeMetadata, MetadataModel)
+    ).toBeFalsy()
   })
   it('checks if the metadata matches corresponding ctype hash', async () => {
     expect(metadata.ctypeHash).toEqual(ctype.hash)
@@ -69,7 +72,7 @@ describe('CType', () => {
     }
     // @ts-expect-error
     delete faultyMetadata.metadata.properties
-    expect(() => CTypeMetadata.check(metadata)).toThrow(
+    expect(() => Verification.verifyCTypeMetadata(metadata)).toThrow(
       SDKErrors.ERROR_OBJECT_MALFORMED()
     )
   })

packages/core/src/ctype/nested.spec.ts
@@ -10,9 +10,9 @@
  */
 
 import type { ICType, IClaim, IClaimContents } from '@kiltprotocol/types'
-import * as CType from './CType'
+import * as CType from './ctype'
 import * as Claim from '../claim'
-import * as CTypeUtils from './CType.utils'
+import * as Verification from './verification'
 
 describe('Nested CTypes', () => {
   const didAlice = 'did:kilt:4p6K4tpdZtY3rNqM2uorQmsS6d3woxtnWMHjtzGftHmDb41N'
@@ -154,7 +154,7 @@ describe('Nested CTypes', () => {
 
   it('verify json-schema validator', () => {
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         nestedCType.schema,
         [passport.schema, kyc.schema],
         claimContents
@@ -173,7 +173,7 @@ describe('Nested CTypes', () => {
       new Error('Nested claim data does not validate against CType')
     )
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         deeplyNestedCType.schema,
         [passport.schema, kyc.schema],
         claimDeepContents
@@ -181,7 +181,7 @@ describe('Nested CTypes', () => {
     ).toBeTruthy()
     ;(claimDeepContents.passport as Record<string, unknown>).fullName = {}
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         deeplyNestedCType.schema,
         [passport.schema, kyc.schema],
         claimDeepContents

packages/core/src/ctype/utils.spec.ts
@@ -12,15 +12,14 @@
 import type { ICType } from '@kiltprotocol/types'
 import { SDKErrors } from '@kiltprotocol/utils'
 import {
-  verifyClaimStructure,
-  verifySchema,
-  verifySchemaWithErrors,
+  verifyClaimAgainstSchema,
+  verifyObjectAgainstSchema,
   verifyStored,
   verifyOwner,
-} from './CType.utils'
-import * as CType from './CType.js'
-import { CTypeModel, CTypeWrapperModel } from './CTypeSchema'
-import { getOwner, isStored } from './CType.chain'
+} from './verification'
+import * as CType from './ctype.js'
+import { CTypeModel, CTypeWrapperModel } from './schemas'
+import { getOwner, isStored } from './chain'
 
 jest.mock('./CType.chain')
 
@@ -68,15 +67,19 @@ const badClaim = {
 
 describe('CTypeUtils', () => {
   it('verifies claims', () => {
-    expect(verifyClaimStructure(goodClaim, ctypeWrapperModel)).toBeTruthy()
-    expect(verifyClaimStructure(badClaim, ctypeWrapperModel)).toBeFalsy()
-    expect(verifySchemaWithErrors(badClaim, CTypeWrapperModel, [])).toBeFalsy()
+    expect(verifyClaimAgainstSchema(goodClaim, ctypeWrapperModel)).toBeTruthy()
+    expect(verifyClaimAgainstSchema(badClaim, ctypeWrapperModel)).toBeFalsy()
+    expect(
+      verifyObjectAgainstSchema(badClaim, CTypeWrapperModel, [])
+    ).toBeFalsy()
     expect(() => {
-      verifyClaimStructure(badClaim, ctypeInput)
+      verifyClaimAgainstSchema(badClaim, ctypeInput)
     }).toThrow(SDKErrors.ERROR_OBJECT_MALFORMED())
   })
   it('verifies ctypes', () => {
-    expect(verifySchema(ctypeWrapperModel, CTypeModel)).toBeTruthy()
+    expect(
+      verifyObjectAgainstSchema(ctypeWrapperModel, CTypeModel)
+    ).toBeTruthy()
   })
 })
 

KILTprotocol/sdk-js

packages/core/src/ctype/index.ts
@@ -5,7 +5,8 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-export * as CType from './CType.js'
-export * as CTypeUtils from './CType.utils.js'
-export * as CTypeMetadata from './CTypeMetadata.js'
-export * as CTypeSchema from './CTypeSchema.js'
+export * from './base.js'
+export * from './utils.js'
+export * from './verification.js'
+export * from './compression.js'
+export * as Schemas from './schemas.js'

packages/core/src/ctype/metadata.spec.ts
@@ -13,10 +13,9 @@
 
 import { SDKErrors } from '@kiltprotocol/utils'
 import type { ICType, ICTypeMetadata } from '@kiltprotocol/types'
-import * as CType from './CType'
-import * as CTypeUtils from './CType.utils'
-import * as CTypeMetadata from './CTypeMetadata'
-import { MetadataModel } from './CTypeSchema'
+import * as CType from './base'
+import * as Verification from './verification'
+import { MetadataModel } from './schemas'
 
 describe('CType', () => {
   const didAlice = 'did:kilt:4p6K4tpdZtY3rNqM2uorQmsS6d3woxtnWMHjtzGftHmDb41N'
@@ -54,10 +53,14 @@ describe('CType', () => {
   })
 
   it('verifies the metadata of a ctype', async () => {
-    expect(() => CTypeMetadata.check(metadata)).not.toThrow()
+    expect(() => Verification.verifyCTypeMetadata(metadata)).not.toThrow()
     expect(metadata.ctypeHash).not.toHaveLength(0)
-    expect(CTypeUtils.verifySchema(metadata, MetadataModel)).toBeTruthy()
-    expect(CTypeUtils.verifySchema(ctypeMetadata, MetadataModel)).toBeFalsy()
+    expect(
+      Verification.verifyObjectAgainstSchema(metadata, MetadataModel)
+    ).toBeTruthy()
+    expect(
+      Verification.verifyObjectAgainstSchema(ctypeMetadata, MetadataModel)
+    ).toBeFalsy()
   })
   it('checks if the metadata matches corresponding ctype hash', async () => {
     expect(metadata.ctypeHash).toEqual(ctype.hash)
@@ -69,7 +72,7 @@ describe('CType', () => {
     }
     // @ts-expect-error
     delete faultyMetadata.metadata.properties
-    expect(() => CTypeMetadata.check(metadata)).toThrow(
+    expect(() => Verification.verifyCTypeMetadata(metadata)).toThrow(
       SDKErrors.ERROR_OBJECT_MALFORMED()
     )
   })

packages/core/src/ctype/nested.spec.ts
@@ -10,9 +10,9 @@
  */
 
 import type { ICType, IClaim, IClaimContents } from '@kiltprotocol/types'
-import * as CType from './CType'
+import * as CType from './base'
 import * as Claim from '../claim'
-import * as CTypeUtils from './CType.utils'
+import * as Verification from './verification'
 
 describe('Nested CTypes', () => {
   const didAlice = 'did:kilt:4p6K4tpdZtY3rNqM2uorQmsS6d3woxtnWMHjtzGftHmDb41N'
@@ -154,7 +154,7 @@ describe('Nested CTypes', () => {
 
   it('verify json-schema validator', () => {
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         nestedCType.schema,
         [passport.schema, kyc.schema],
         claimContents
@@ -173,7 +173,7 @@ describe('Nested CTypes', () => {
       new Error('Nested claim data does not validate against CType')
     )
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         deeplyNestedCType.schema,
         [passport.schema, kyc.schema],
         claimDeepContents
@@ -181,7 +181,7 @@ describe('Nested CTypes', () => {
     ).toBeTruthy()
     ;(claimDeepContents.passport as Record<string, unknown>).fullName = {}
     expect(
-      CTypeUtils.validateNestedSchemas(
+      Verification.verifyClaimAgainstNestedSchemas(
         deeplyNestedCType.schema,
         [passport.schema, kyc.schema],
         claimDeepContents

packages/core/src/ctype/utils.spec.ts
@@ -12,15 +12,14 @@
 import type { ICType } from '@kiltprotocol/types'
 import { SDKErrors } from '@kiltprotocol/utils'
 import {
-  verifyClaimStructure,
-  verifySchema,
-  verifySchemaWithErrors,
+  verifyClaimAgainstSchema,
+  verifyObjectAgainstSchema,
   verifyStored,
   verifyOwner,
-} from './CType.utils'
-import * as CType from './CType.js'
-import { CTypeModel, CTypeWrapperModel } from './CTypeSchema'
-import { getOwner, isStored } from './CType.chain'
+} from './verification'
+import * as CType from './base.js'
+import { CTypeModel, CTypeWrapperModel } from './schemas'
+import { getOwner, isStored } from './chain'
 
 jest.mock('./CType.chain')
 
@@ -68,15 +67,19 @@ const badClaim = {
 
 describe('CTypeUtils', () => {
   it('verifies claims', () => {
-    expect(verifyClaimStructure(goodClaim, ctypeWrapperModel)).toBeTruthy()
-    expect(verifyClaimStructure(badClaim, ctypeWrapperModel)).toBeFalsy()
-    expect(verifySchemaWithErrors(badClaim, CTypeWrapperModel, [])).toBeFalsy()
+    expect(verifyClaimAgainstSchema(goodClaim, ctypeWrapperModel)).toBeTruthy()
+    expect(verifyClaimAgainstSchema(badClaim, ctypeWrapperModel)).toBeFalsy()
+    expect(
+      verifyObjectAgainstSchema(badClaim, CTypeWrapperModel, [])
+    ).toBeFalsy()
     expect(() => {
-      verifyClaimStructure(badClaim, ctypeInput)
+      verifyClaimAgainstSchema(badClaim, ctypeInput)
     }).toThrow(SDKErrors.ERROR_OBJECT_MALFORMED())
   })
   it('verifies ctypes', () => {
-    expect(verifySchema(ctypeWrapperModel, CTypeModel)).toBeTruthy()
+    expect(
+      verifyObjectAgainstSchema(ctypeWrapperModel, CTypeModel)
+    ).toBeTruthy()
   })
 })
 