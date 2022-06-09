d0a885f9a15c97ab87a356edf48fdeb26ac28e35
Timo Welde <tjwelde@gmail.com>
2022-06-09 19:17:25 +0200

    chore: disable some eslint rules in bundle tests

--- tests/bundle-test.js
+++ tests/bundle-test.js
@@ -5,6 +5,8 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
+/* eslint-disable no-console, @typescript-eslint/explicit-function-return-type */
+
 const {
   Claim,
   Attestation,
