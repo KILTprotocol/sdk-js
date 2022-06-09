b509a2a454d783043b338fdc4fee9ff02195768b
Timo Welde <tjwelde@gmail.com>
2022-06-09 16:10:05 +0200

    feat(req4att): consolidate into one file

--- packages/core/src/requestforattestation/index.ts
+++ packages/core/src/requestforattestation/index.ts
@@ -5,7 +5,4 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-export * from './base.js'
-export * from './compression.js'
-export * from './utils.js'
-export * from './verification.js'
+export * from './RequestForAttestation.js'
