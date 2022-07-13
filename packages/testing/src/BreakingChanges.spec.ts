a001c5542bb56b43d67c85e2f7239142d85358fc
Timo Welde <tjwelde@gmail.com>
2022-07-13 18:26:03 +0200

    test: mock randomness & fix after merge

--- packages/testing/src/BreakingChanges.spec.ts
+++ packages/testing/src/BreakingChanges.spec.ts
@@ -12,9 +12,7 @@
 import {
   Did,
   Utils,
-  VerificationKeyType,
   Message,
-  MessageBodyType,
   IDidResolver,
   DidResolvedDetails,
   ResolvedDidKey,
@@ -23,6 +21,7 @@ import {
   DidKey,
   MessageBody,
 } from '@kiltprotocol/sdk-js'
+import nacl from 'tweetnacl'
 import { makeEncryptionKeyTool } from './TestUtils'
 
 function makeLightDidFromSeed(seed: string) {
@@ -33,12 +32,15 @@ function makeLightDidFromSeed(seed: string) {
 
   const keypair = keyring.addFromUri(seed, undefined, 'sr25519')
 
+  // Mock nacl randombytes, so that the nonce and ciphertext stay the same between runs
+  jest.spyOn(nacl, 'randomBytes').mockReturnValue(new Uint8Array(24).fill(42))
+
   const { keypair: encKeypair, encrypt } = makeEncryptionKeyTool(seed)
 
   const did = Did.LightDidDetails.fromDetails({
     authenticationKey: {
       publicKey: keypair.publicKey,
-      type: VerificationKeyType.Sr25519,
+      type: 'sr25519',
     },
     encryptionKey: encKeypair,
     serviceEndpoints: [
@@ -102,9 +104,19 @@ describe('Breaking Changes', () => {
         '0xa748f38e896ddc52b6e5cc5baa754f7f841381ef32bf1d86d51026857c6c05dc'
       )
 
+      // Mock Date object for message.createdAt property
+      jest.useFakeTimers().setSystemTime(1657727664899)
+
+      // Mock UUID.generate for message.id property
+      jest
+        .spyOn(Utils.UUID, 'generate')
+        .mockReturnValue(
+          '0x072678802b3e7807fbec86cc93328d52de76e0249011918d4f96835fa0923b8e'
+        )
+
       const message = new Message(
         {
-          type: MessageBodyType.REQUEST_TERMS,
+          type: 'request-terms',
           content: {
             cTypeHash: '0x1234',
           },
@@ -113,10 +125,6 @@ describe('Breaking Changes', () => {
         bobDid.uri
       )
 
-      // createdAt and messageId are different each time
-      message.createdAt = 1657212991479
-      message.messageId =
-        '0x072678802b3e7807fbec86cc93328d52de76e0249011918d4f96835fa0923b8e'
       expect(message).toMatchInlineSnapshot(`
         Message {
           "body": Object {
@@ -125,7 +133,7 @@ describe('Breaking Changes', () => {
             },
             "type": "request-terms",
           },
-          "createdAt": 1657212991479,
+          "createdAt": 1657727664899,
           "messageId": "0x072678802b3e7807fbec86cc93328d52de76e0249011918d4f96835fa0923b8e",
           "receiver": "did:kilt:light:004rzcHqKvv6TbsA46VpG53JrvdzRr6ccyboUNeCGTvDw2AozU:z147wqPsQV7GGKBxQGmTMeDTTVPBvZ1AdEf99W5vCASMxUTrTY233fstS4x3nkmngDgoWfHq8ej5AfRsoAQczQbFdscM7KQa8QEcqC8LnPtkgJEAXmoWf13FQNR4HQb3vjz1fkAvuRagmE6Dfgt4nVXv5ku2jkj8aR8irPg2wa33VhNi8NT5m7QJjsFeaUpKQpsUwMdk9UWjCdSS9gy6yrsRaLUVLcRvhqteuy72N4tAFWaCdF2YJsHYAwJdnML1x9gM3hMo7yzskmrY9RwkmeeuXGV9GJoEFUTsXWk12mDea2E1f7FJBYgnJp8Vp5ve1HVtZVzum",
           "sender": "did:kilt:light:004rn1Xps9QSMiAb2aMoKQbAred94wh1iLjGmNBYy1FVCS81ry:z147wqPsQV7GGKBxQGmTMeDTTVP41uiX9BXTq4uqLWGHgTe9Vx2hK81GGQvXGRee5m2EiEqd1fSMzDpgNEFEUNHEBAGsTd8bqDWPhapBetBQ8wAtG7wHj1XwCswvSNp79Sg6NMuwKs2oco4fnHK85cKvmkMnArR3kbHPjAfqeg9wdQ2VtPN3DkjzLgsMbjHH7G9BJCBMRKtJJMuSBs3BWagzY2QCGGC54k5XN3XWiF1QCMG2edoyvShyxhVNyay7PwGg2y5xCK4d6Dg4g2ucq5iidDppobcUFCU7rXqVnrHsALjwe6S4gnF6cC5ZrkkpHxLw94fXV",
@@ -142,16 +150,10 @@ describe('Breaking Changes', () => {
         }
       )
 
-      // ciphertext and nonce are always random
-      // @ts-ignore
-      encrypted.ciphertext = null
-      // @ts-ignore
-      encrypted.nonce = null
-
       expect(encrypted).toMatchInlineSnapshot(`
         Object {
-          "ciphertext": null,
-          "nonce": null,
+          "ciphertext": "0x62a1b62183259d33d1fdc35293843b15a4f2d914786050424594c89f07ccbc2f643df95ba030281079a92215c66d76a74b39e6013cb8a5e1459a6853fdbfb522bff1295cbf9e5e2d6947de5974dafe6de32e6d42e2c349f07b5bb934a504d0a227d502e920cadfae44869ed041d86916cee71e0619650f15a1a04f27751f0839e8f3c629d626a54f489c4170b3ffefe118039925124acba91c097bf4d8c582f26c8a3b0f1dba93bf8da6b8f2a2a4303ac529c7028aec026f6baee8eab6fa3194e52cd6c0ce5f7f5d5b906dba5cb98b8365dd6757aa299ff92ff83657931b0eb81b0af191643e832f72faa2b092f287adac41985aa162d00478ec540f034de7f3cfc1c25d2d67a6e35c9f1478eb90d16b2172627b7d9c5baccfa91cd334a83f6dc3b6c8147c1ad48e67c0561ca4e611f62e12e4db9f0eaf6c3bad1154853b080973d148a36089cbaf4ed0d9f79366346ed150e0aef647b7817381ac2fab579cd987a095dc89aaf774490f2772d8531b7ad8005892fc20ce8333fb4d3b1b69397c333cce7961f0a9a004939cf6021f5e404859c26b3a217e099c17008b805147c3b447201a0bee2d71279a347bd7fcc9c34450cfc16510a1b95239afbaa6a2795945777f265bb43d30134d6631934d311030233c73c4475d27b15acdadc22e34339080ae8e1f3c0569fe026569901f88dfef75227b338d6d37877c784e18c51eba5809967f99efe58bb703c9c3dc988dd1f514c408e96d88a3ad384c18964963d7aafc1a82af79c0c67be6bef4218c7b5a965d3a2e18fea0824eb9c6c3fae99f70ad7f2932920e112cecf23357b5e3c1ff48c60fe578599afe16389301f24e7198e41858f8e998d8a3c3bd1957e719d0dd37c3587aa4d989fd9a2bdc7d374c9a747c74d1811f64cf0d7bb93754927e78da5794a083bd5978ebb013346c7d59dc9232a0a64874f2ea9339b367d58c7b8567e25f2a9abffac3f5281b8cc2fc063a61caa37ca8ed2f87d2d564dbbcc1a7c524863a2dcfa77c3b1c457de8f57f9d8644421886d91c304aec587c6f30a911eb078670227bb986da8b2947af44416cef6bdba5307bcfb241cd6a3b388d09a856ec70c5d7aba98c6e2e3291a289e4b0834939b1e1954327f0d747ea846c9bc53f4f925b64cc27a6f21c3f91b979694d9d8453bd4dbe00f94ca36014e168bbbf52b66a2aa7948623b8e74f29083e9b6590d4ec42f41ffb04a552847ab72b7301fcb28634e4cc384e14542a96f441035ffe5cb15f09d358b1b7da95192c698af1414cc14a651da135665c764498e8616a816998c657a6a1435f15736822f48546d7925bdc8672b65e0b0a9dc92b9f1b8a348e509baf26171f456c2a3b0369e3a15ca50410e6bdb1bafff7b404156c52cc9713513272157eb1c0c40088cf670a",
+          "nonce": "0x2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a",
           "receivedAt": undefined,
           "receiverKeyUri": "did:kilt:light:004rzcHqKvv6TbsA46VpG53JrvdzRr6ccyboUNeCGTvDw2AozU:z147wqPsQV7GGKBxQGmTMeDTTVPBvZ1AdEf99W5vCASMxUTrTY233fstS4x3nkmngDgoWfHq8ej5AfRsoAQczQbFdscM7KQa8QEcqC8LnPtkgJEAXmoWf13FQNR4HQb3vjz1fkAvuRagmE6Dfgt4nVXv5ku2jkj8aR8irPg2wa33VhNi8NT5m7QJjsFeaUpKQpsUwMdk9UWjCdSS9gy6yrsRaLUVLcRvhqteuy72N4tAFWaCdF2YJsHYAwJdnML1x9gM3hMo7yzskmrY9RwkmeeuXGV9GJoEFUTsXWk12mDea2E1f7FJBYgnJp8Vp5ve1HVtZVzum#encryption",
           "senderKeyUri": "did:kilt:light:004rn1Xps9QSMiAb2aMoKQbAred94wh1iLjGmNBYy1FVCS81ry:z147wqPsQV7GGKBxQGmTMeDTTVP41uiX9BXTq4uqLWGHgTe9Vx2hK81GGQvXGRee5m2EiEqd1fSMzDpgNEFEUNHEBAGsTd8bqDWPhapBetBQ8wAtG7wHj1XwCswvSNp79Sg6NMuwKs2oco4fnHK85cKvmkMnArR3kbHPjAfqeg9wdQ2VtPN3DkjzLgsMbjHH7G9BJCBMRKtJJMuSBs3BWagzY2QCGGC54k5XN3XWiF1QCMG2edoyvShyxhVNyay7PwGg2y5xCK4d6Dg4g2ucq5iidDppobcUFCU7rXqVnrHsALjwe6S4gnF6cC5ZrkkpHxLw94fXV#encryption",
@@ -160,7 +162,7 @@ describe('Breaking Changes', () => {
     })
     it('does not break for all message types', () => {
       const message: MessageBody = {
-        type: MessageBodyType.REQUEST_TERMS,
+        type: 'request-terms',
         content: {
           cTypeHash: '0x1234',
         },
