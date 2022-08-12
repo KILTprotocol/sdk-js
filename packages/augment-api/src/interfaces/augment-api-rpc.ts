58970daec6e8d280a1a8555a03e08c47944c6c2a
Antonio Antonino <antonio@kilt.io>
2022-08-12 11:50:13 +0200

    chore: update package reference for type generation

--- packages/augment-api/src/interfaces/augment-api-rpc.ts
+++ packages/augment-api/src/interfaces/augment-api-rpc.ts
@@ -5,7 +5,7 @@
 // this is required to allow for ambient/previous definitions
 import '@polkadot/rpc-core/types/jsonrpc';
 
-import type { PublicCredentialFilter } from '@kiltprotocol/augment-api/extraDefs';
+import type { PublicCredentialFilter } from './extraDefs';
 import type { AugmentedRpc } from '@polkadot/rpc-core/types';
 import type { Metadata, StorageKey } from '@polkadot/types';
 import type { Bytes, HashMap, Json, Null, Option, Text, U256, U64, Vec, bool, f64, u32, u64 } from '@polkadot/types-codec';
--- packages/augment-api/src/interfaces/augment-api-runtime.ts
+++ packages/augment-api/src/interfaces/augment-api-runtime.ts
@@ -5,7 +5,7 @@
 // this is required to allow for ambient/previous definitions
 import '@polkadot/api-base/types/calls';
 
-import type { RawDidLinkedInfo } from '@kiltprotocol/augment-api/extraDefs';
+import type { RawDidLinkedInfo } from './extraDefs';
 import type { ApiTypes, AugmentedCall, DecoratedCallBase } from '@polkadot/api-base/types';
 import type { Bytes, Null, Option, Text, Vec, u32 } from '@polkadot/types-codec';
 import type { AnyNumber, ITuple } from '@polkadot/types-codec/types';
