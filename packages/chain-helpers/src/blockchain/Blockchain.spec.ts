723e64bfe4725dcb6aaf456e52e2a344c4ccaf3b
Raphael Flechtner <raphael@kilt.io>
2022-09-07 09:37:58 +0200

    chore: increase test coverage

--- packages/chain-helpers/src/blockchain/Blockchain.spec.ts
+++ packages/chain-helpers/src/blockchain/Blockchain.spec.ts
@@ -9,15 +9,24 @@
  * @group unit/blockchain
  */
 
-/* eslint-disable dot-notation */
+import Keyring from '@polkadot/keyring'
+
 import { ApiMocks } from '@kiltprotocol/testing'
 import { ConfigService } from '@kiltprotocol/config'
+import type { KeyringPair } from '@kiltprotocol/types'
+import { SDKErrors } from '@kiltprotocol/utils'
 
-import { IS_FINALIZED, parseSubscriptionOptions } from './Blockchain'
+import {
+  IS_FINALIZED,
+  IS_IN_BLOCK,
+  IS_READY,
+  parseSubscriptionOptions,
+  signAndSubmitTx,
+} from './Blockchain'
 
 let api: any
 
-beforeAll(() => {
+beforeEach(() => {
   api = ApiMocks.getMockedApi()
   ConfigService.set({ api })
 })
@@ -72,6 +81,67 @@ describe('Blockchain', () => {
         rejectOn: expect.any(Function),
         timeout: 10,
       })
+    })
+  })
+
+  describe('submitSignedTx', () => {
+    let pair: KeyringPair
+
+    beforeAll(async () => {
+      pair = new Keyring().addFromUri('//Alice')
+    })
+
+    it('allows waiting for finalization', async () => {
+      api.__setDefaultResult({ isFinalized: true })
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      expect(
+        await signAndSubmitTx(tx, pair, { resolveOn: IS_FINALIZED })
+      ).toHaveProperty('isFinalized', true)
+    })
+
+    it('allows waiting for in block', async () => {
+      api.__setDefaultResult({ isInBlock: true })
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      expect(
+        await signAndSubmitTx(tx, pair, { resolveOn: IS_IN_BLOCK })
+      ).toHaveProperty('isInBlock', true)
+    })
+
+    it('allows waiting for ready', async () => {
+      api.__setDefaultResult({ isReady: true })
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      expect(
+        await signAndSubmitTx(tx, pair, { resolveOn: IS_READY })
+      ).toHaveProperty('status.isReady', true)
+    })
+
+    it('rejects on error condition', async () => {
+      api.__setDefaultResult({ isInvalid: true })
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      await expect(
+        signAndSubmitTx(tx, pair, { resolveOn: IS_FINALIZED })
+      ).rejects.toHaveProperty('isError', true)
+    })
+
+    it('throws if subscriptions not supported', async () => {
+      // @ts-ignore
+      api.hasSubscriptions = false
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      await expect(
+        signAndSubmitTx(tx, pair, { resolveOn: IS_FINALIZED })
+      ).rejects.toThrow(SDKErrors.SubscriptionsNotSupportedError)
+    })
+
+    it('rejects if disconnected', async () => {
+      api.__setDefaultResult({ isReady: true })
+      api.once.mockImplementation((ev: string, callback: () => void) => {
+        // mock disconnect 500 ms after submission
+        if (ev === 'disconnected') setTimeout(callback, 500)
+      })
+      const tx = api.tx.balances.transfer('abcdef', 50)
+      await expect(
+        signAndSubmitTx(tx, pair, { resolveOn: IS_FINALIZED })
+      ).rejects.toHaveProperty('internalError', expect.any(Error))
     })
   })
 })
