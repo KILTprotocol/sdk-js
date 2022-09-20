6a58f09fdd72cfb002f9eb57440d43ad6cc3d7d7
Antonio Antonino <antonio@kilt.io>
2022-09-20 13:43:33 +0200

    Final tweaks

--- packages/augment-api/src/index.ts
+++ packages/augment-api/src/index.ts
@@ -5,13 +5,9 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import './interfaces/augment-api-consts.js'
-import './interfaces/augment-api-query.js'
-import './interfaces/augment-api-rpc.js'
-// FIXME: manually added, as it is not added by the generation script
-import './interfaces/augment-api-runtime.js'
-import './interfaces/augment-api-tx.js'
+import './interfaces/augment-api.js'
 import './interfaces/augment-types.js'
+import './interfaces/registry.js'
 import './interfaces/types-lookup.js'
 
 export * from './types.js'
