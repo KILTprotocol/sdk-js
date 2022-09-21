92f2496fe20fe16088257d95691e3895dfb5392f
Antonio <antonio@kilt.io>
2022-09-21 08:28:15 +0200

    feat: add DID RPC decorations (#637)
    * wip
    
    * Final tweaks
    
    * Fix lint issue
    
    * New defs with minor pinning to type definitions
    
    * Update 9.x polkadot dependencies

--- packages/augment-api/src/interfaces/did/index.ts
+++ packages/augment-api/src/interfaces/did/index.ts
@@ -1,5 +1,4 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
-export * from './extraDefs/types.js';
-export * from './did/types.js';
+export * from './types.js';
