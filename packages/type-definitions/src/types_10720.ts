ac30d4104a37c33ef8fbed227372850446ba54d2
William Freudenberger <w.freude@icloud.com>
2022-10-27 10:15:09 +0200

    fix: DID API types (#670)
    * fix: did API types
    * fix: chain spec names
    * refactor: remove deprecated Raw chain types
    * chore: re-run api augmentation
    * chore: safer conversion to text
    
    Co-authored-by: Raphael Flechtner <raphael@kilt.io>

--- packages/type-definitions/src/types_10720.ts
+++ packages/type-definitions/src/types_10720.ts
@@ -14,21 +14,7 @@ export const types10720: RegistryTypes = {
     identifier: 'AccountId32',
     accounts: 'Vec<DidApiAccountId>',
     w3n: 'Option<Text>',
-    serviceEndpoints: 'Vec<RawServiceEndpoints>',
-    details: 'RawDidDetails',
-  },
-  RawServiceEndpoints: {
-    id: 'Text',
-    serviceTypes: 'Vec<Text>',
-    urls: 'Vec<Text>',
-  },
-  RawDidDetails: {
-    authenticationKey: 'Hash',
-    keyAgreementKeys: 'BTreeSet<Hash>',
-    delegationKey: 'Option<Hash>',
-    attestationKey: 'Option<Hash>',
-    publicKeys: 'BTreeMap<Hash, DidDidDetailsDidPublicKeyDetails<BlockNumber>>',
-    lastTxCounter: 'BlockNumber',
-    deposit: 'KiltSupportDeposit<AccountId32, Balance>',
+    serviceEndpoints: 'Vec<DidServiceEndpointsDidEndpoint>',
+    details: 'DidDidDetails',
   },
 }
