7ae6b0a04a0cb0f32d7440bc8612e1e9e3958536
Raphael Flechtner <raphael@kilt.io>
2022-11-23 18:06:38 +0100

    fix: make sure @polkadot/types/lookup is defined before exporting its types

--- packages/augment-api/src/types.ts
+++ packages/augment-api/src/types.ts
@@ -5,6 +5,7 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
+import './interfaces/types-lookup.js'
 /* eslint-disable import/no-extraneous-dependencies */
 export * from '@polkadot/types/lookup'
 export * from './interfaces/index.js'
