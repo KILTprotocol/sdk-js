213370d6590234b6b5a0ee862a8ce39fedc37c06
Raphael Flechtner <raphael@kilt.io>
2023-01-09 11:56:15 -0500

    fix: linter errors

--- packages/vc-export/src/vc-js/context/index.ts
+++ packages/vc-export/src/vc-js/context/index.ts
@@ -5,7 +5,7 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import context from './context.js'
+import { context } from './context.js'
 import { KILT_CREDENTIAL_CONTEXT_URL } from '../../constants.js'
 
 export const validationContexts = { [KILT_CREDENTIAL_CONTEXT_URL]: context }
