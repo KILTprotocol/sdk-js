3dfae54eaea9e1d30bc99475e3c08a48049a5643
Raphael Flechtner <raphael@kilt.io>
2023-03-04 19:13:47 +0100

    feat: proof creation via vc-js suite

--- packages/vc-export/src/vc-js/suites/KiltAttestationProofV1.ts
+++ packages/vc-export/src/vc-js/suites/KiltAttestationProofV1.ts
@@ -10,7 +10,14 @@
 
 import jsigs from 'jsonld-signatures'
 import { ApiPromise } from '@polkadot/api'
-import { verifyProof } from '../../KiltAttestationProofV1.js'
+import { u8aToHex } from '@polkadot/util'
+import {
+  AttestationHandler,
+  calculateRootHash,
+  finalizeProof,
+  initializeProof,
+  verifyProof,
+} from '../../KiltAttestationProofV1.js'
 import { checkStatus } from '../../KiltRevocationStatusV1.js'
 import type {
   KiltAttestationProofV1,
@@ -24,10 +31,19 @@ const {
 
 export class KiltAttestationV1Suite extends LinkedDataProof {
   private api: ApiPromise
+  private transactionHandler?: AttestationHandler
+  private pendingSubmissions = new Map<string, ReturnType<AttestationHandler>>()
 
-  constructor({ api }: { api: ApiPromise }) {
+  constructor({
+    api,
+    transactionHandler,
+  }: {
+    api: ApiPromise
+    transactionHandler?: AttestationHandler
+  }) {
     super({ type: ATTESTATION_PROOF_V1_TYPE })
     this.api = api
+    this.transactionHandler = transactionHandler
   }
 
   /**
@@ -86,15 +102,55 @@ export class KiltAttestationV1Suite extends LinkedDataProof {
   /**
    * @inheritdoc
    */
-  public async createProof({}: {
+  public async createProof({
+    document,
+  }: {
     document: object
     purpose?: jsigs.purposes.ProofPurpose
     documentLoader?: jsigs.DocumentLoader
     expansionMap?: jsigs.ExpansionMap
   }): Promise<KiltAttestationProofV1> {
-    // eslint-disable-next-line class-methods-use-this
-    throw new Error(
-      'Credential issuance through vc-js is not supported. For credential issuance, use @kiltprotocol/sdk-js and export your KILT credential to a VC representation using @kiltprotocol/vc-export'
+    if (!this.transactionHandler) {
+      throw new Error(
+        'suite must be configured with a transactionHandler for proof generation'
+      )
+    }
+    const [proof, submissionArgs] = initializeProof(
+      document as VerifiableCredential
     )
+    const [rootHash] = submissionArgs
+    this.pendingSubmissions.set(
+      u8aToHex(rootHash as Uint8Array),
+      this.transactionHandler(this.api.tx.attestation.add(...submissionArgs))
+    )
+    return proof
+  }
+
+  /**
+   * @param credential
+   */
+  public async finalizeProof(
+    credential: VerifiableCredential
+  ): Promise<VerifiableCredential> {
+    const { proof } = credential
+    const rootHash = u8aToHex(calculateRootHash(credential, proof!))
+    const submissionPromise = this.pendingSubmissions.get(rootHash)
+    if (!submissionPromise) {
+      throw new Error('no submission found for this proof')
+    }
+    const {
+      blockHash,
+      timestamp = (await this.api.query.timestamp.now.at(blockHash)).toNumber(),
+    } = await submissionPromise.catch((e) => {
+      this.pendingSubmissions.delete(rootHash)
+      throw new Error(`Promise rejected with ${e}`)
+    })
+    const updated = finalizeProof(credential, proof!, {
+      blockHash,
+      timestamp,
+      genesisHash: this.api.genesisHash,
+    })
+    this.pendingSubmissions.delete(rootHash)
+    return updated
   }
 }
