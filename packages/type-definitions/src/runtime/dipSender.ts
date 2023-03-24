7bf2424888508d5d0fbc574343ab6b58adf085ae
Antonio Antonino <antonio@kilt.io>
2023-03-24 16:09:11 +0100

    Add type definitions for the new dipSender runtime API

---
+++ packages/type-definitions/src/runtime/dipSender.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2018-2023, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import type { DefinitionsCall } from '@polkadot/types/types'
+
+export const calls: DefinitionsCall = {
+  DipSender: [
+    {
+      methods: {
+        generate_proof: {
+          description:
+            'Generate a Merkle proof for the DIP protocol for the specified DID and public keys.',
+          params: [
+            {
+              name: 'identifier',
+              type: 'DidIdentifierOf',
+            },
+            {
+              name: 'keys',
+              type: 'BTreeSet<Hash>',
+            },
+          ],
+          type: 'Result<CompleteMerkleProof, ()>',
+        },
+      },
+      version: 1,
+    },
+  ],
+}
