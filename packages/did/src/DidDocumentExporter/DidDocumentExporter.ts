9b111e1acced4f4631f411214a8023dac22c0f49
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2023-03-27 16:58:39 +0200

    refactor!: make all key ids absolute uris on conformant did doc (#742)

--- packages/did/src/DidDocumentExporter/DidDocumentExporter.ts
+++ packages/did/src/DidDocumentExporter/DidDocumentExporter.ts
@@ -12,6 +12,7 @@ import type {
   ConformingDidDocument,
   DidResourceUri,
   JsonLDDidDocument,
+  UriFragment,
 } from '@kiltprotocol/types'
 import {
   encryptionKeyTypesMap,
@@ -30,6 +31,13 @@ function exportToJsonDidDocument(did: DidDocument): ConformingDidDocument {
     service = [],
   } = did
 
+  function toAbsoluteUri(keyId: UriFragment): DidResourceUri {
+    if (keyId.startsWith(controller)) {
+      return keyId as DidResourceUri
+    }
+    return `${controller}${keyId}`
+  }
+
   const verificationMethod: ConformingDidDocument['verificationMethod'] = [
     ...authentication,
     ...assertionMethod,
@@ -43,7 +51,7 @@ function exportToJsonDidDocument(did: DidDocument): ConformingDidDocument {
       }))
     )
     .map(({ id, type, publicKey }) => ({
-      id: `${controller}${id}` as DidResourceUri,
+      id: toAbsoluteUri(id),
       controller,
       type,
       publicKeyBase58: base58Encode(publicKey),
@@ -57,15 +65,15 @@ function exportToJsonDidDocument(did: DidDocument): ConformingDidDocument {
   return {
     id: controller,
     verificationMethod,
-    authentication: [authentication[0].id],
+    authentication: [toAbsoluteUri(authentication[0].id)],
     ...(assertionMethod[0] && {
-      assertionMethod: [assertionMethod[0].id],
+      assertionMethod: [toAbsoluteUri(assertionMethod[0].id)],
     }),
     ...(capabilityDelegation[0] && {
-      capabilityDelegation: [capabilityDelegation[0].id],
+      capabilityDelegation: [toAbsoluteUri(capabilityDelegation[0].id)],
     }),
     ...(keyAgreement.length > 0 && {
-      keyAgreement: [keyAgreement[0].id],
+      keyAgreement: [toAbsoluteUri(keyAgreement[0].id)],
     }),
     ...(service.length > 0 && {
       service: service.map((endpoint) => ({
