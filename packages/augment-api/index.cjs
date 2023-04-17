a4c6d338b40ec2d555332132a624501c17907806
Raphael Flechtner <raphael@kilt.io>
2023-04-17 11:46:23 +0200

    chore: add license header to index files

--- packages/augment-api/index.cjs
+++ packages/augment-api/index.cjs
@@ -1,1 +1,8 @@
+/**
+ * Copyright (c) 2018-2023, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
 module.exports = {}
