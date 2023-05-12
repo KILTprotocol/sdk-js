1b4941c1d7fdaaf8293bed59582340443a45914e
Antonio <antonio@kilt.io>
2023-05-12 14:40:53 +0300

    feat: DID merkle proofs (#744)
    * Add type definitions for the new dipSender runtime API
    
    * Update types with new spec 11000
    
    * Update type definitions
    
    * Change keyIds input type

---
+++ packages/type-definitions/src/types_11000.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2018-2023, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+import type { RegistryTypes } from '@polkadot/types/types'
+
+import { types10900 } from './types_10900.js'
+
+export const types11000: RegistryTypes = {
+  ...types10900,
+  // DipSender state_call
+  CompleteMerkleProof: {
+    root: 'MerkleRoot',
+    proof: 'MerkleProof',
+  },
+  MerkleRoot: 'Hash',
+  MerkleProof: {
+    blinded: 'BlindedLeaves',
+    revealed: 'RevealedLeaves',
+  },
+  BlindedLeaves: 'Vec<BlindedValue>',
+  BlindedValue: 'Bytes',
+  RevealedLeaves: 'Vec<RevealedLeaf>',
+  RevealedLeaf: {
+    _enum: {
+      KeyReference: '(KeyReferenceKey, KeyReferenceValue)',
+      KeyDetails: '(KeyDetailsKey, KeyDetailsValue)',
+    },
+  },
+  KeyReferenceKey: '(KeyId, KeyRelationship)',
+  KeyReferenceValue: 'Null',
+  KeyDetailsKey: 'KeyId',
+  KeyDetailsValue: 'DidDidDetailsDidPublicKeyDetails',
+  KeyId: 'Hash',
+  KeyRelationship: {
+    _enum: {
+      Encryption: 'Null',
+      Verification: 'VerificationRelationship',
+    },
+  },
+  VerificationRelationship: {
+    _enum: [
+      'Authentication',
+      'CapabilityDelegation',
+      'CapabilityInvocation',
+      'AssertionMethod',
+    ],
+  },
+}
