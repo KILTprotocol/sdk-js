85be995ffbade7d6a2edcc34693b009848c9f7ac
Raphael Flechtner <raphael@kilt.io>
2023-05-31 18:29:22 +0200

    feat: export imported types

---
+++ packages/types/src/Imported.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2018-2023, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+export type { HexString } from '@polkadot/util/types'
+export type { AnyNumber, AnyJson } from '@polkadot/types/types/codec'
+export type { BN } from '@polkadot/util'
+export type { Prefix } from '@polkadot/util-crypto/address/types'
