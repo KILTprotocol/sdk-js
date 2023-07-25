53967a1445e2e6db920e9c59b938ce5bfdb03577
Raphael Flechtner <raphael@kilt.io>
2023-07-25 14:49:09 +0200

    chore: remove coverage from integration as it does not work

--- tests/integration/jest.config.integration.esm.js
+++ tests/integration/jest.config.integration.esm.js
@@ -1,20 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: "node",
   clearMocks: true,
   // Parachain block time is 12s
   testTimeout: 120_000,
   maxWorkers: 3,
-  roots: ['dist'],
+  roots: ["dist"],
   transform: {},
-  coverageDirectory: 'coverage',
   moduleDirectories: [
     "node_modules",
   ],
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "packages/testing",
-    "packages/type-definitions",
-    "packages/types",
-    "packages/augment-api"
-  ]
 }
--- tests/integration/jest.config.integration.js
+++ tests/integration/jest.config.integration.js
@@ -1,21 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   clearMocks: true,
   // Parachain block time is 12s
   testTimeout: 120_000,
   maxWorkers: 3,
-  testPathIgnorePatterns: ['dist'],
+  testPathIgnorePatterns: ["dist"],
   resolver: "ts-jest-resolver",
-  coverageDirectory: 'coverage',
   moduleDirectories: [
     "node_modules",
   ],
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "packages/testing",
-    "packages/type-definitions",
-    "packages/types",
-    "packages/augment-api"
-  ]
 }
