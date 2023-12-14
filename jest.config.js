f38294311aef1068f9449f2cfe4631ef8164bda6
Raphael Flechtner <raphael@kilt.io>
2023-12-14 11:26:59 +0100

    test: interim reduction of coverage goals

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,10 @@ const common = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   },
   collectCoverageFrom: [
