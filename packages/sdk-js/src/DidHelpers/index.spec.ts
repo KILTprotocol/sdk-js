c30a482d36eb3329a20a0f497604da04e7574021
Raphael Flechtner <raphael@kilt.io>
2024-07-10 10:09:54 +0200

    test: some fixes to did helper tests

--- packages/sdk-js/src/DidHelpers/index.spec.ts
+++ packages/sdk-js/src/DidHelpers/index.spec.ts
@@ -1,21 +1,31 @@
-import { ConfigService } from '../index.js'
+/**
+ * Copyright (c) 2018-2024, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
 
+import { SubmittableResult } from '@polkadot/api'
+
+import { authorizeTx, resolver } from '@kiltprotocol/did'
+import type { DidDocument, KiltKeyringPair } from '@kiltprotocol/types'
+import { Crypto } from '@kiltprotocol/utils'
+
+import { ConfigService } from '../index.js'
 import {
   ApiMocks,
   createLocalDemoFullDidFromKeypair,
 } from '../../../../tests/testUtils/index.js'
+import { transact } from './index.js'
+import { TransactionResult } from './interfaces.js'
 
 jest.mock('@kiltprotocol/did', () => {
   return {
     ...jest.requireActual('@kiltprotocol/did'),
     resolver: { resolve: jest.fn() },
+    authorizeTx: jest.fn(),
   }
 })
-import { resolver } from '@kiltprotocol/did'
-import { transact } from './index.js'
-import { Crypto } from '@kiltprotocol/utils'
-import { DidDocument, KiltKeyringPair } from '@kiltprotocol/types'
-// import { SubmittableResult } from '@polkadot/api'
 
 const mockedApi = ApiMocks.createAugmentedApi()
 
@@ -29,7 +39,33 @@ describe('transact', () => {
     //   .mockResolvedValue(mockedApi.createType(''))
 
     keypair = Crypto.makeKeypairFromUri('//Alice')
-    didDocument = await createLocalDemoFullDidFromKeypair(keypair)
+    const { id, verificationMethod, authentication } =
+      await createLocalDemoFullDidFromKeypair(keypair, {
+        verificationRelationships: new Set(['assertionMethod']),
+      })
+    didDocument = {
+      id,
+      authentication,
+      assertionMethod: authentication,
+      verificationMethod: verificationMethod?.filter(
+        (vm) => vm.id === authentication![0]
+      ),
+    }
+    jest
+      .mocked(authorizeTx)
+      .mockImplementation(async (did, ex, sig, sub, { txCounter } = {}) => {
+        // if(!sig.find(i => i.id)) {}
+        return mockedApi.tx.did.submitDidCall(
+          {
+            did: keypair.address,
+            txCounter: txCounter ?? 0,
+            call: ex,
+            blockNumber: 0,
+            submitter: sub,
+          },
+          { ed25519: new Uint8Array(64) }
+        )
+      })
     jest.mocked(resolver).resolve.mockImplementation((did) => {
       if (did === didDocument.id) {
         return { didDocument } as any
@@ -39,7 +75,7 @@ describe('transact', () => {
   })
 
   it('creates a tx and checks status', async () => {
-    const { txHex } = await transact({
+    const { txHex, checkResult } = await transact({
       didDocument,
       api: mockedApi,
       submitter: keypair,
@@ -52,7 +88,7 @@ describe('transact', () => {
       expectedEvents: [
         { section: 'attestation', method: 'AttestationCreated' },
       ],
-    }).getSubmittable()
+    }).getSubmittable({ signSubmittable: false })
 
     expect(txHex).toContain('0x')
     const parsed = mockedApi.tx(txHex)
@@ -60,16 +96,34 @@ describe('transact', () => {
       section: 'did',
       method: 'submitDidCall',
     })
-    // expect(
-    //   checkResult(
-    //     new SubmittableResult({
-    //       blockNumber: mockedApi.createType('BlockNumber', 1000),
-    //       status: mockedApi.createType('ExtrinsicStatus', {
-    //         inBlock: new Uint8Array(32).fill(2),
-    //       }),
-    //       txHash: parsed.hash,
-    //     })
-    //   )
-    // )
+
+    await expect(
+      checkResult(
+        new SubmittableResult({
+          blockNumber: mockedApi.createType('BlockNumber', 1000),
+          status: mockedApi.createType('ExtrinsicStatus', {
+            inBlock: new Uint8Array(32).fill(2),
+          }),
+          txHash: parsed.hash,
+          events: [
+            mockedApi.createType('EventRecord', {
+              phase: { ApplyExtrinsic: 0 },
+              event: { section: 'attestation', method: 'AttestationCreated' },
+            }),
+          ],
+        })
+      )
+    ).resolves.toMatchObject<Partial<TransactionResult>>({
+      status: 'confirmed',
+      asConfirmed: expect.objectContaining({
+        txHash: parsed.hash.toHex(),
+        block: {
+          hash: mockedApi
+            .createType('Hash', new Uint8Array(32).fill(2))
+            .toHex(),
+          number: 1000n,
+        },
+      }),
+    })
   })
 })
