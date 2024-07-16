c2070326111d88201c62ecbb82612139d14852bb
Raphael Flechtner <raphael@kilt.io>
2024-07-16 18:20:00 +0200

    test: run service unit tests for fragment- and full ids

--- packages/sdk-js/src/DidHelpers/service.spec.ts
+++ packages/sdk-js/src/DidHelpers/service.spec.ts
@@ -5,7 +5,7 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import type { DidDocument, KiltKeyringPair } from '@kiltprotocol/types'
+import type { DidDocument, DidUrl, KiltKeyringPair } from '@kiltprotocol/types'
 import { Crypto } from '@kiltprotocol/utils'
 import {
   ApiMocks,
@@ -20,102 +20,105 @@ jest.mock('./transact.js')
 const mockedTransact = jest.mocked(transact)
 const mockedApi = ApiMocks.createAugmentedApi()
 
-describe('service management', () => {
-  let didDocument: DidDocument
-  let keypair: KiltKeyringPair
-  beforeAll(async () => {
-    ConfigService.set({ api: mockedApi })
+describe.each(['#my_service', 'did:kilt:4abctest#my_service'])(
+  'service management with id %s',
+  (serviceId) => {
+    let didDocument: DidDocument
+    let keypair: KiltKeyringPair
+    beforeAll(async () => {
+      ConfigService.set({ api: mockedApi })
 
-    keypair = Crypto.makeKeypairFromUri('//Alice')
-    const { id, verificationMethod, authentication } =
-      await createLocalDemoFullDidFromKeypair(keypair, {
-        verificationRelationships: new Set(['assertionMethod']),
-      })
-    didDocument = {
-      id,
-      authentication,
-      assertionMethod: authentication,
-      verificationMethod: verificationMethod?.filter(
-        (vm) => vm.id === authentication![0]
-      ),
-    }
-  })
-
-  it('creates an add service tx', async () => {
-    addService({
-      didDocument,
-      api: mockedApi,
-      submitter: keypair,
-      signers: [keypair],
-      service: {
-        id: '#my_service',
-        type: ['http://schema.org/EmailService'],
-        serviceEndpoint: ['mailto:info@kilt.io'],
-      },
+      keypair = Crypto.makeKeypairFromUri('//Alice')
+      const { id, verificationMethod, authentication } =
+        await createLocalDemoFullDidFromKeypair(keypair, {
+          verificationRelationships: new Set(['assertionMethod']),
+        })
+      didDocument = {
+        id,
+        authentication,
+        assertionMethod: authentication,
+        verificationMethod: verificationMethod?.filter(
+          (vm) => vm.id === authentication![0]
+        ),
+      }
     })
 
-    expect(mockedTransact).toHaveBeenLastCalledWith(
-      expect.objectContaining<Partial<Parameters<typeof transact>[0]>>({
-        call: expect.any(Object),
-        expectedEvents: expect.arrayContaining([
-          {
-            section: 'did',
-            method: 'DidUpdated',
-          },
-        ]),
+    it('creates an add service tx', async () => {
+      addService({
         didDocument,
         api: mockedApi,
         submitter: keypair,
         signers: [keypair],
-      })
-    )
-    expect(mockedTransact.mock.lastCall?.[0].call.toHuman()).toMatchObject({
-      method: {
-        args: {
-          service_endpoint: {
-            id: 'my_service',
-            serviceTypes: ['http://schema.org/EmailService'],
-            urls: ['mailto:info@kilt.io'],
-          },
+        service: {
+          id: serviceId as DidUrl,
+          type: ['http://schema.org/EmailService'],
+          serviceEndpoint: ['mailto:info@kilt.io'],
         },
-        section: 'did',
-        method: 'addServiceEndpoint',
-      },
-    })
-  })
+      })
 
-  it('creates a remove service tx', async () => {
-    removeService({
-      didDocument,
-      api: mockedApi,
-      submitter: keypair,
-      signers: [keypair],
-      id: '#my_service',
-    })
-
-    expect(mockedTransact).toHaveBeenLastCalledWith(
-      expect.objectContaining<Partial<Parameters<typeof transact>[0]>>({
-        call: expect.any(Object),
-        expectedEvents: expect.arrayContaining([
-          {
-            section: 'did',
-            method: 'DidUpdated',
+      expect(mockedTransact).toHaveBeenLastCalledWith(
+        expect.objectContaining<Partial<Parameters<typeof transact>[0]>>({
+          call: expect.any(Object),
+          expectedEvents: expect.arrayContaining([
+            {
+              section: 'did',
+              method: 'DidUpdated',
+            },
+          ]),
+          didDocument,
+          api: mockedApi,
+          submitter: keypair,
+          signers: [keypair],
+        })
+      )
+      expect(mockedTransact.mock.lastCall?.[0].call.toHuman()).toMatchObject({
+        method: {
+          args: {
+            service_endpoint: {
+              id: 'my_service',
+              serviceTypes: ['http://schema.org/EmailService'],
+              urls: ['mailto:info@kilt.io'],
+            },
           },
-        ]),
+          section: 'did',
+          method: 'addServiceEndpoint',
+        },
+      })
+    })
+
+    it('creates a remove service tx', async () => {
+      removeService({
         didDocument,
         api: mockedApi,
         submitter: keypair,
         signers: [keypair],
+        id: serviceId as DidUrl,
       })
-    )
-    expect(mockedTransact.mock.lastCall?.[0].call.toHuman()).toMatchObject({
-      method: {
-        args: {
-          service_id: 'my_service',
+
+      expect(mockedTransact).toHaveBeenLastCalledWith(
+        expect.objectContaining<Partial<Parameters<typeof transact>[0]>>({
+          call: expect.any(Object),
+          expectedEvents: expect.arrayContaining([
+            {
+              section: 'did',
+              method: 'DidUpdated',
+            },
+          ]),
+          didDocument,
+          api: mockedApi,
+          submitter: keypair,
+          signers: [keypair],
+        })
+      )
+      expect(mockedTransact.mock.lastCall?.[0].call.toHuman()).toMatchObject({
+        method: {
+          args: {
+            service_id: 'my_service',
+          },
+          section: 'did',
+          method: 'removeServiceEndpoint',
         },
-        section: 'did',
-        method: 'removeServiceEndpoint',
-      },
+      })
     })
-  })
-})
+  }
+)
