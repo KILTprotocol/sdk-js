fd3c7dfec0f757a45e49231cafd86dd215120a76
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2025-01-16 23:16:00 +0100

    test: fix tests & ci (#932)
    * chore: replace all uses of tx.balances.transfer() (#903)
    * ci: run tests on prs to v0-lts
    * ci: update actions (backport of #871)
    * fix: update playwright dep
    * fix: rename Docker image (#902)
    * chore: pull image via ecr pull cache (#921)
    ---------
    Co-authored-by: Andrés <105802444+kilted-andres@users.noreply.github.com>
    Co-authored-by: Antonio <antonio@kilt.io>
    Co-authored-by: Gerawork Aynekulu <ggera@users.noreply.github.com>

--- tests/integration/utils.ts
+++ tests/integration/utils.ts
@@ -169,7 +169,9 @@ export async function endowAccounts(
 ): Promise<void> {
   const api = ConfigService.get('api')
   const transactions = await Promise.all(
-    addresses.map((address) => api.tx.balances.transfer(address, ENDOWMENT))
+    addresses.map((address) =>
+      api.tx.balances.transferKeepAlive(address, ENDOWMENT)
+    )
   )
   const batch = api.tx.utility.batchAll(transactions)
   await Blockchain.signAndSubmitTx(batch, faucet, { resolveOn })
@@ -180,7 +182,7 @@ export async function fundAccount(
   amount: BN
 ): Promise<void> {
   const api = ConfigService.get('api')
-  const transferTx = api.tx.balances.transfer(address, amount)
+  const transferTx = api.tx.balances.transferKeepAlive(address, amount)
   await submitTx(transferTx, devFaucet)
 }
 
