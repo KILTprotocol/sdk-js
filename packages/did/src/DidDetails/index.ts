d92b480a26cfab33d8a55f1be2ec6e18e1602a92
Skedley <48058984+Dudleyneedham@users.noreply.github.com>
2025-02-07 16:53:26 +0100

    chore: Kilt foundation (#940)

--- packages/did/src/DidDetails/index.ts
+++ packages/did/src/DidDetails/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018-2024, BOTLabs GmbH.
+ * Copyright (c) 2025, KILT Foundation.
  *
  * This source code is licensed under the BSD 4-Clause "Original" license
  * found in the LICENSE file in the root directory of this source tree.
